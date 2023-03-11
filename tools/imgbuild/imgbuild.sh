4fe28bdacfba72b797d15b46bca57c095e167caa
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-03-11 16:27:25 +0100

    tools: imgbuild: Use FAT12 when possible to save space
    FAT12 wastes less space than FAT16 for small image sizes.
    
    FAT12: 64 KiB to 32 MiB
    FAT16: 16 MiB to 2 GiB (enough for any NDS game, probably)

--- tools/imgbuild/imgbuild.sh
+++ tools/imgbuild/imgbuild.sh
@@ -17,13 +17,24 @@ else
     IMAGE_SIZE=$3
 fi
 
-MIN_IMAGE_SIZE=$(expr 16 \* 1024 \* 1024) # 16 MiB
-IMAGE_SIZE=$(( $IMAGE_SIZE > $MIN_IMAGE_SIZE ? $IMAGE_SIZE : $MIN_IMAGE_SIZE ))
+# Anything over this size will be formatted as FAT16. Anything below it, as
+# FAT12. FAT12 uses space more efficiently, but it has a very low max disk size
+# limit.
+FAT12_MAX_SIZE=$(expr 32 \* 1024 \* 1024) # 32 MiB
+
+if [ $IMAGE_SIZE -lt $FAT12_MAX_SIZE ]; then
+    FAT_VERSION=12
+    FAT12_MIN_SIZE=$(expr 64 \* 1024) # 64 KiB is the minimum for FAT12
+    IMAGE_SIZE=$(( $IMAGE_SIZE > $FAT12_MIN_SIZE ? $IMAGE_SIZE : $FAT12_MIN_SIZE ))
+else
+    # The minimum size for FAT16 is 16 MiB
+    FAT_VERSION=16
+fi
+
 SECTOR_SIZE=512
 IMAGE_SIZE_KIB=$(expr $IMAGE_SIZE / 1024)
 
 rm -rf "$IMAGE_PATH"
 
-# Use FAT16 instead of FAT32
-mkdosfs -F 16 -S $SECTOR_SIZE -C "$IMAGE_PATH" "$IMAGE_SIZE_KIB"
+mkdosfs -F $FAT_VERSION -S $SECTOR_SIZE -C "$IMAGE_PATH" "$IMAGE_SIZE_KIB"
 mcopy -i "$IMAGE_PATH" -s "$ROOT_DIR_PATH"/* ::/
