de680fe6e93d3619dad6fec57dd27f4992c0aabe
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-03-27 18:36:21 +0100

    mkfatimg: Add missing license headers to files

--- tools/mkfatimg/source/integer.h
+++ tools/mkfatimg/source/integer.h
@@ -1,6 +1,22 @@
-/*-------------------------------------------*/
-/* Integer type definitions for FatFs module */
-/*-------------------------------------------*/
+/*------------------------------------------------------------------------/
+/  Integer type definitions for FatFs module                              /
+/-------------------------------------------------------------------------/
+/
+/ Copyright (C) 2019, ChaN, all right reserved.
+/
+/ FatFs module is an open source software. Redistribution and use of FatFs in
+/ source and binary forms, with or without modification, are permitted provided
+/ that the following condition is met:
+/
+/ 1. Redistributions of source code must retain the above copyright notice,
+/    this condition and the following disclaimer.
+/
+/ This software is provided by the copyright holder and contributors "AS IS"
+/ and any warranties related to this software are DISCLAIMED.
+/ The copyright owner or contributors be NOT LIABLE for any damages caused
+/ by use of this software.
+/
+/----------------------------------------------------------------------------*/
 
 #ifndef FF_INTEGER
 #define FF_INTEGER
