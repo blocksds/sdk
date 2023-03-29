6d8b1be564d089700f112828f448bcfe0d4832f2
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-03-29 02:37:01 +0100

    tests: Add test to exit to loader
    It checks information about the DLDI driver, the signature of the
    bootstub struct, and it lets the user try an ARM7 or ARM9 exit to
    loader.

---
+++ tests/exit_to_loader/source/main.c
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: CC0-1.0
+//
+// SPDX-FileContributor: Antonio Niño Díaz, 2023
+
+#include <stdio.h>
+
+#include <nds.h>
+#include <nds/arm9/dldi.h>
+
+int main(int argc, char **argv)
+{
+    consoleDemoInit();
+
+    printf("Exit to loader\n");
+    printf("--------------\n");
+    printf("\n");
+    printf("L+R+START+SELECT: ARM7 exit\n");
+    printf("A+B:              ARM9 exit\n");
+    printf("\n");
+
+    printf("DLDI name:\n");
+    printf("%s\n", io_dldi_data->friendlyName);
+    printf("\n");
+    printf("DSi mode: %d\n", isDSiMode());
+    printf("\n");
+
+    extern char *fake_heap_end;
+    struct __bootstub *bootstub = (void *)fake_heap_end;
+
+    printf("bootstub:      0x%X\n", (uintptr_t)bootstub);
+    printf("fake_heap_end: 0x%X\n", (uintptr_t)fake_heap_end);
+    printf("\n");
+    printf("bootsig: 0x%llX %s\n", bootstub->bootsig,
+           bootstub->bootsig == BOOTSIG ? "OK" : "BAD");
+    printf("\n");
+    printf("arm9reboot: 0x%X\n", (uintptr_t)bootstub->arm9reboot);
+    printf("arm7reboot: 0x%X\n", (uintptr_t)bootstub->arm9reboot);
+    printf("bootsize:   0x%lX\n", bootstub->bootsize);
+
+    fflush(stdout);
+
+    while (1)
+    {
+        swiWaitForVBlank();
+
+        scanKeys();
+
+        uint32_t keys = keysHeld();
+        if ((keys & KEY_A) && (keys & KEY_B))
+            break;
+    }
+
+    return 0;
+}
