d3678c0b499f5b09c7a758c605d9bb20f83d184f
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-04-10 12:33:53 +0100

    templates: Add initial DLDI template files
    The DLDI template of the NDS examples of devkitPro doesn't have a
    license. However, the dldi_stub.s file of libnds is licensed under the
    Zlib license. It can be used as base.

---
+++ templates/dldi/readme.rst
@@ -0,0 +1,16 @@
+DLDI template
+=============
+
+This template builds a DLDI driver.
+
+To build an ARM7 DLDI driver, just run:
+
+.. code:: bash
+
+    make
+
+To build an ARM9 DLDI driver, run:
+
+.. code:: bash
+
+    make DLDI_ARM9=1
---
+++ templates/dldi/source/dldi_stub.s
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: Zlib
+//
+// Copyright (C) 2006-2016 Michael Chisholm (Chishm)
+// Copyright (C) 2006-2016 Dave Murphy (WinterMute)
+
+#include <nds/arm9/dldi_asm.h>
+
+	.align	4
+	.arm
+	.global _io_dldi_stub
+@---------------------------------------------------------------------------------
+
+.equ DLDI_ALLOCATED_SPACE,		16384
+
+_io_dldi_stub:
+
+dldi_start:
+
+@---------------------------------------------------------------------------------
+@ Driver patch file standard header -- 16 bytes
+	.word	0xBF8DA5ED		@ Magic number to identify this region
+	.asciz	" Chishm"		@ Identifying Magic string (8 bytes with null terminator)
+	.byte	0x01			@ Version number
+	.byte	DLDI_SIZE_16KB	@16KiB	@ Log [base-2] of the size of this driver in bytes.
+	.byte	0x00			@ Sections to fix
+	.byte 	DLDI_SIZE_16KB	@16KiB	@ Log [base-2] of the allocated space in bytes.
+	
+@---------------------------------------------------------------------------------
+@ Text identifier - can be anything up to 47 chars + terminating null -- 16 bytes
+	.align	4
+	.asciz "Default (No interface)"
+
+@---------------------------------------------------------------------------------
+@ Offsets to important sections within the data	-- 32 bytes
+	.align	6
+	.word   dldi_start		@ data start
+	.word   dldi_end		@ data end
+	.word	0x00000000		@ Interworking glue start	-- Needs address fixing
+	.word	0x00000000		@ Interworking glue end
+	.word   0x00000000		@ GOT start					-- Needs address fixing
+	.word   0x00000000		@ GOT end
+	.word   0x00000000		@ bss start					-- Needs setting to zero
+	.word   0x00000000		@ bss end
+
+@---------------------------------------------------------------------------------
+@ DISC_INTERFACE data -- 32 bytes
+	.ascii	"DLDI"				@ ioType
+	.word	0x00000000			@ Features
+	.word	_DLDI_startup			@ 
+	.word	_DLDI_isInserted		@ 
+	.word	_DLDI_readSectors		@   Function pointers to standard device driver functions
+	.word	_DLDI_writeSectors		@ 
+	.word	_DLDI_clearStatus		@ 
+	.word	_DLDI_shutdown			@ 
+	
+@---------------------------------------------------------------------------------
+
+_DLDI_startup:
+_DLDI_isInserted:
+_DLDI_readSectors:
+_DLDI_writeSectors:
+_DLDI_clearStatus:
+_DLDI_shutdown:
+	mov		r0, #0x00				@ Return false for every function
+	bx		lr
+
+
+
+@---------------------------------------------------------------------------------
+	.align
+	.pool
+
+dldi_data_end:
+
+@ Pad to end of allocated space
+.space DLDI_ALLOCATED_SPACE - (dldi_data_end - dldi_start)	
+
+dldi_end:
+	.end
+@---------------------------------------------------------------------------------
