94fc1cb4e8e1c05978ab11bf74c78ed2ec386d4f
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-09-23 03:13:58 +0100

    examples: Add key input example

---
+++ examples/key_input/source/main.c
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: CC0-1.0
+//
+// SPDX-FileContributor: Antonio Niño Díaz, 2023
+
+#include <stdio.h>
+
+#include <nds.h>
+
+int main(int argc, char **argv)
+{
+    consoleDemoInit();
+
+    while (1)
+    {
+        swiWaitForVBlank();
+
+        scanKeys();
+
+        uint16_t keys_down = keysDown();
+        uint16_t keys_held = keysHeld();
+        uint16_t keys_up = keysUp();
+
+        // Clear console
+        printf("\x1b[2J");
+
+        printf("Press START+SELECT to exit\n\n");
+
+        if ((keys_held & (KEY_START | KEY_SELECT)) == (KEY_START | KEY_SELECT))
+            break;
+
+        printf("Key input test\n\n");
+
+        const char *key_names[15] = {
+            "A", "B", "Select", "Start", "Right", "Left", "Up", "Down", "R",
+            "L", "X", "Y", "Touch", "Lid", "Debug"
+        };
+
+        printf("Down: ");
+        for (int i = 0; i <= 14; i++)
+        {
+            if (keys_down & BIT(i))
+                printf("%s ", key_names[i]);
+        }
+
+        printf("\n\nHeld: ");
+        for (int i = 0; i <= 14; i++)
+        {
+            if (keys_held & BIT(i))
+                printf("%s ", key_names[i]);
+        }
+
+        printf("\n\nUp: ");
+        for (int i = 0; i <= 14; i++)
+        {
+            if (keys_up & BIT(i))
+                printf("%s ", key_names[i]);
+        }
+    }
+
+    return 0;
+}
