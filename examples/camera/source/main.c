50ef254c92310e4cc14ae10bf602a1b3c54b5e8d
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-10-12 23:19:20 +0100

    examples: Uniformize code style across all examples

--- examples/camera/source/main.c
+++ examples/camera/source/main.c
@@ -20,182 +20,217 @@
 
 // YUV<->RGB conversion
 
-static inline int clamp(int val, int min, int max) {
-	return val < min ? min : (val > max) ? max : val;
+static inline int clamp(int val, int min, int max)
+{
+    return val < min ? min : (val > max) ? max : val;
 }
 
 #define YUV_TO_R(Y, Cr) clamp(Y + Cr + (Cr >> 2) + (Cr >> 3) + (Cr >> 5), 0, 0xFF)
 #define YUV_TO_G(Y, Cb, Cr) \
-	clamp(Y - ((Cb >> 2) + (Cb >> 4) + (Cb >> 5)) - ((Cr >> 1) + (Cr >> 3) + (Cr >> 4) + (Cr >> 5)), 0, 0xFF)
+    clamp(Y - ((Cb >> 2) + (Cb >> 4) + (Cb >> 5)) - ((Cr >> 1) + (Cr >> 3) + (Cr >> 4) + (Cr >> 5)), 0, 0xFF)
 #define YUV_TO_B(Y, Cb) clamp(Y + Cb + (Cb >> 1) + (Cb >> 2) + (Cb >> 6), 0, 0xFF)
 
 ITCM_CODE
-static void convertYuv422ToRgb(u16 *yuv, u8 *rgb, int width, int height) {
-	for(int py = 0; py < height; py++) {
-		for(int px = 0; px < width; px += 2) {
-			u8 *val = (u8 *)(yuv + py * width + px);
+static void convertYuv422ToRgb(u16 *yuv, u8 *rgb, int width, int height)
+{
+    for (int py = 0; py < height; py++)
+    {
+        for (int px = 0; px < width; px += 2)
+        {
+            u8 *val = (u8 *)(yuv + py * width + px);
 
-			// Get YUV values
-			int Y1 = val[0];
-			int Cb = val[1] - 0x80;
-			int Y2 = val[2];
-			int Cr = val[3] - 0x80;
+            // Get YUV values
+            int Y1 = val[0];
+            int Cb = val[1] - 0x80;
+            int Y2 = val[2];
+            int Cr = val[3] - 0x80;
 
-			u8 *dst = rgb + py * (width * 3) + px * 3;
-			// First pixel R, G, B
-			dst[0] = YUV_TO_R(Y1, Cr);
-			dst[1] = YUV_TO_G(Y1, Cb, Cr);
-			dst[2] = YUV_TO_B(Y1, Cb);
-			// Second pixel R, G, B
-			dst[3] = YUV_TO_R(Y2, Cr);
-			dst[4] = YUV_TO_G(Y2, Cb, Cr);
-			dst[5] = YUV_TO_B(Y2, Cb);
-		}
-	}
+            u8 *dst = rgb + py * (width * 3) + px * 3;
+            // First pixel R, G, B
+            dst[0] = YUV_TO_R(Y1, Cr);
+            dst[1] = YUV_TO_G(Y1, Cb, Cr);
+            dst[2] = YUV_TO_B(Y1, Cb);
+            // Second pixel R, G, B
+            dst[3] = YUV_TO_R(Y2, Cr);
+            dst[4] = YUV_TO_G(Y2, Cb, Cr);
+            dst[5] = YUV_TO_B(Y2, Cb);
+        }
+    }
 }
 
 // Main program logic
 
-int getImageNumber() {
-	int highest = -1;
+int getImageNumber(void)
+{
+    int highest = -1;
 
-	DIR *pdir = opendir("/DCIM/100DSI00");
-	if(pdir == NULL) {
-		printf("Unable to open directory");
-		return -1;
-	} else {
-		while(true) {
-			struct dirent *pent = readdir(pdir);
-			if(pent == NULL)
-				break;
+    DIR *pdir = opendir("/DCIM/100DSI00");
+    if (pdir == NULL)
+    {
+        printf("Unable to open directory");
+        return -1;
+    }
+    else
+    {
+        while (true)
+        {
+            struct dirent *pent = readdir(pdir);
+            if (pent == NULL)
+                break;
 
-			if(strncmp(pent->d_name, "IMG_", 4) == 0) {
-				int val = atoi(pent->d_name + 4);
-				if(val > highest)
-					highest = val;
-			}
-		}
-		closedir(pdir);
-	}
+            if (strncmp(pent->d_name, "IMG_", 4) == 0)
+            {
+                int val = atoi(pent->d_name + 4);
+                if (val > highest)
+                    highest = val;
+            }
+        }
+        closedir(pdir);
+    }
 
-	return highest + 1;
+    return highest + 1;
 }
 
-static int exitOnError(void) {
-	while (!(keysDown() & KEY_START)) {
-		swiWaitForVBlank();
-		scanKeys();
-	}
-	return 1;
+static int exitOnError(void)
+{
+    while (!(keysDown() & KEY_START))
+    {
+        swiWaitForVBlank();
+        scanKeys();
+    }
+    return 1;
 }
 
-int main(int argc, char **argv) {
-	// Set up console
-	consoleDemoInit();
-	vramSetBankA(VRAM_A_MAIN_BG);
-	videoSetMode(MODE_5_2D);
-	int bg3Main = bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 1, 0);
+int main(int argc, char **argv)
+{
+    // Set up console
+    consoleDemoInit();
+    vramSetBankA(VRAM_A_MAIN_BG);
+    videoSetMode(MODE_5_2D);
+    int bg3Main = bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 1, 0);
 
-	printf("Camera Test\n");
+    printf("Camera Test\n");
 
-	if (!isDSiMode()) {
-		printf("Must launch in DSi mode!\n");
-		return exitOnError();
-	}
+    if (!isDSiMode())
+    {
+        printf("Must launch in DSi mode!\n");
+        return exitOnError();
+    }
 
-	bool fatInited = fatInitDefault();
-	if(fatInited) {
-		mkdir("/DCIM", 0777);
-		mkdir("/DCIM/100DSI00", 0777);
-	} else {
-		printf("FAT init failed, photos cannot\nbe saved.\n");
-	}
+    bool fatInited = fatInitDefault();
+    if(fatInited)
+    {
+        mkdir("/DCIM", 0777);
+        mkdir("/DCIM/100DSI00", 0777);
+    }
+    else
+    {
+        printf("FAT init failed, photos cannot\nbe saved.\n");
+    }
 
-	printf("Initializing camera...\n");
-	if (!cameraInit()) {
-		printf("Camera initialization failed!\n");
-		return exitOnError();
-	}
+    printf("Initializing camera...\n");
+    if (!cameraInit())
+    {
+        printf("Camera initialization failed!\n");
+        return exitOnError();
+    }
 
-	printf("Selecting camera...\n");
-	u8 camera = CAMERA_OUTER;
-	if (!cameraSelect(camera)) {
-		printf("Camera selection failed!\n");
-		return exitOnError();
-	}
+    printf("Selecting camera...\n");
+    u8 camera = CAMERA_OUTER;
+    if (!cameraSelect(camera))
+    {
+        printf("Camera selection failed!\n");
+        return exitOnError();
+    }
 
-	printf("\nA to swap");
-	if(fatInited) printf(", L or R to take photo\n");
-	printf("\n");
+    printf("\nA to swap");
+    if (fatInited)
+        printf(", L or R to take photo\n");
+    printf("\n");
 
-	while (true) {
-		u16 pressed;
-		do {
-			swiWaitForVBlank();
-			// If no preview transfer ongoing, start a new preview transfer.
-			if(!ndmaBusy(CAMERA_NDMA_CHANNEL) || !cameraTransferActive()) {
-				cameraStartTransfer(bgGetGfxPtr(bg3Main), MCUREG_APT_SEQ_CMD_PREVIEW, CAMERA_NDMA_CHANNEL);
-			}
-			scanKeys();
-			pressed = keysDown();
-		} while(!pressed);
+    while (true)
+    {
+        u16 pressed;
+        do
+        {
+            swiWaitForVBlank();
+            // If no preview transfer ongoing, start a new preview transfer.
+            if(!ndmaBusy(CAMERA_NDMA_CHANNEL) || !cameraTransferActive())
+            {
+                cameraStartTransfer(bgGetGfxPtr(bg3Main),
+                                    MCUREG_APT_SEQ_CMD_PREVIEW,
+                                    CAMERA_NDMA_CHANNEL);
+            }
+            scanKeys();
+            pressed = keysDown();
+        } while(!pressed);
 
-		if(pressed & KEY_A) {
-			// Wait for previous transfer to finish
-			while(ndmaBusy(CAMERA_NDMA_CHANNEL))
-				swiWaitForVBlank();
-			cameraStopTransfer();
+        if (pressed & KEY_A)
+        {
+            // Wait for previous transfer to finish
+            while (ndmaBusy(CAMERA_NDMA_CHANNEL))
+                swiWaitForVBlank();
+            cameraStopTransfer();
 
-			// Switch camera
-			camera = camera == CAMERA_INNER ? CAMERA_OUTER : CAMERA_INNER;
-			if (!cameraSelect(camera)) {
-				printf("Camera selection failed!\n");
-				return exitOnError();
-			}
+            // Switch camera
+            camera = camera == CAMERA_INNER ? CAMERA_OUTER : CAMERA_INNER;
+            if (!cameraSelect(camera))
+            {
+                printf("Camera selection failed!\n");
+                return exitOnError();
+            }
 
-			printf("Swapped to %s camera\n", camera == CAMERA_INNER ? "inner" : "outer");
-		} else if(fatInited && (pressed & (KEY_L | KEY_R))) {
-			printf("Capturing... ");
+            printf("Swapped to %s camera\n",
+                   camera == CAMERA_INNER ? "inner" : "outer");
+        }
+        else if(fatInited && (pressed & (KEY_L | KEY_R)))
+        {
+            printf("Capturing... ");
 
-			// Wait for previous transfer to finish
-			while(ndmaBusy(CAMERA_NDMA_CHANNEL))
-				swiWaitForVBlank();
+            // Wait for previous transfer to finish
+            while (ndmaBusy(CAMERA_NDMA_CHANNEL))
+                swiWaitForVBlank();
 
-			u16 *yuv = (u16 *)malloc(640 * 480 * sizeof(u16));
-			u8 *rgb = (u8 *)malloc(640 * 480 * 3);
-			if (yuv == NULL || rgb == NULL) {
-				printf("Could not allocate capture arrays!\n");
-				if (yuv != NULL) free(yuv);
-				if (rgb != NULL) free(rgb);
-				continue;
-			}
+            u16 *yuv = (u16 *)malloc(640 * 480 * sizeof(u16));
+            u8 *rgb = (u8 *)malloc(640 * 480 * 3);
+            if (yuv == NULL || rgb == NULL)
+            {
+                printf("Could not allocate capture arrays!\n");
+                if (yuv != NULL)
+                    free(yuv);
+                if (rgb != NULL)
+                    free(rgb);
+                continue;
+            }
 
-			// Run a capture transfer.
-			cameraStartTransfer(yuv, MCUREG_APT_SEQ_CMD_CAPTURE, CAMERA_NDMA_CHANNEL);
-			while(ndmaBusy(CAMERA_NDMA_CHANNEL))
-				swiWaitForVBlank();
-			cameraStopTransfer();
+            // Run a capture transfer.
+            cameraStartTransfer(yuv, MCUREG_APT_SEQ_CMD_CAPTURE, CAMERA_NDMA_CHANNEL);
+            while (ndmaBusy(CAMERA_NDMA_CHANNEL))
+                swiWaitForVBlank();
+            cameraStopTransfer();
 
-			// Convert YUV data to RGB.
-			printf("Done!\nSaving PNG..");
-			convertYuv422ToRgb(yuv, rgb, 640, 480);
-			free(yuv);
-			printf(". ");
+            // Convert YUV data to RGB.
+            printf("Done!\nSaving PNG..");
+            convertYuv422ToRgb(yuv, rgb, 640, 480);
+            free(yuv);
+            printf(". ");
 
-			// Save RGB data as PNG file.
-			char imgName[32];
-			sprintf(imgName, "/DCIM/100DSI00/IMG_%04d.PNG", getImageNumber());
-			stbi_write_png(imgName, 640, 480, 3, rgb, 640 * 3);
-			free(rgb);
+            // Save RGB data as PNG file.
+            char imgName[32];
+            sprintf(imgName, "/DCIM/100DSI00/IMG_%04d.PNG", getImageNumber());
+            stbi_write_png(imgName, 640, 480, 3, rgb, 640 * 3);
+            free(rgb);
 
-			printf("Done!\nSaved to:\n%s\n\n", imgName);
-		} else if(pressed & KEY_START) {
-			while(ndmaBusy(CAMERA_NDMA_CHANNEL))
-				swiWaitForVBlank();
-			break;
-		}
-	}
+            printf("Done!\nSaved to:\n%s\n\n", imgName);
+        }
+        else if(pressed & KEY_START)
+        {
+            while (ndmaBusy(CAMERA_NDMA_CHANNEL))
+                swiWaitForVBlank();
+            break;
+        }
+    }
 
-	cameraDeinit();
-	return 0;
+    cameraDeinit();
+    return 0;
 }
--- examples/libxm7/arm7/source/main.c
+++ examples/libxm7/arm7/source/main.c
@@ -1,114 +1,111 @@
-/*---------------------------------------------------------------------------------
+/*
+    default ARM7 core
 
-	default ARM7 core
+        Copyright (C) 2005 - 2010
+        Michael Noland (joat)
+        Jason Rogers (dovoto)
+        Dave Murphy (WinterMute)
 
-		Copyright (C) 2005 - 2010
-		Michael Noland (joat)
-		Jason Rogers (dovoto)
-		Dave Murphy (WinterMute)
+    This software is provided 'as-is', without any express or implied
+    warranty.  In no event will the authors be held liable for any
+    damages arising from the use of this software.
 
-	This software is provided 'as-is', without any express or implied
-	warranty.  In no event will the authors be held liable for any
-	damages arising from the use of this software.
+    Permission is granted to anyone to use this software for any
+    purpose, including commercial applications, and to alter it and
+    redistribute it freely, subject to the following restrictions:
 
-	Permission is granted to anyone to use this software for any
-	purpose, including commercial applications, and to alter it and
-	redistribute it freely, subject to the following restrictions:
+    1.    The origin of this software must not be misrepresented; you
+        must not claim that you wrote the original software. If you use
+        this software in a product, an acknowledgment in the product
+        documentation would be appreciated but is not required.
 
-	1.	The origin of this software must not be misrepresented; you
-		must not claim that you wrote the original software. If you use
-		this software in a product, an acknowledgment in the product
-		documentation would be appreciated but is not required.
+    2.    Altered source versions must be plainly marked as such, and
+        must not be misrepresented as being the original software.
 
-	2.	Altered source versions must be plainly marked as such, and
-		must not be misrepresented as being the original software.
+    3.    This notice may not be removed or altered from any source
+        distribution.
+*/
 
-	3.	This notice may not be removed or altered from any source
-		distribution.
-
----------------------------------------------------------------------------------*/
 #include <dswifi7.h>
 #include <libxm7.h>
 #include <nds.h>
 
 // Assign FIFO_USER_07 channel to libxm7
-#define FIFO_XM7	(FIFO_USER_07)
+#define FIFO_XM7    (FIFO_USER_07)
 
-//---------------------------------------------------------------------------------
-void VblankHandler(void) {
-//---------------------------------------------------------------------------------
-	Wifi_Update();
+void VblankHandler(void)
+{
+    Wifi_Update();
 }
 
-
-//---------------------------------------------------------------------------------
-void VcountHandler() {
-//---------------------------------------------------------------------------------
-	inputGetAndSend();
+void VcountHandler(void)
+{
+    inputGetAndSend();
 }
 
 volatile bool exitflag = false;
 
-//---------------------------------------------------------------------------------
-void powerButtonCB() {
-//---------------------------------------------------------------------------------
-	exitflag = true;
-}
-
-void XM7_Value32Handler(u32 command, void* userdata)
+void powerButtonCB(void)
 {
-	XM7_ModuleManager_Type *module = (XM7_ModuleManager_Type *)command;
-
-	if (module == NULL)
-		XM7_StopModule();
-	else
-		XM7_PlayModule(module);
+    exitflag = true;
 }
 
-//---------------------------------------------------------------------------------
-int main() {
-//---------------------------------------------------------------------------------
-	// clear sound registers
-	dmaFillWords(0, (void*)0x04000400, 0x100);
+void XM7_Value32Handler(u32 command, void *userdata)
+{
+    XM7_ModuleManager_Type *module = (XM7_ModuleManager_Type *)command;
 
-	REG_SOUNDCNT |= SOUND_ENABLE;
-	writePowerManagement(PM_CONTROL_REG, ( readPowerManagement(PM_CONTROL_REG) & ~PM_SOUND_MUTE ) | PM_SOUND_AMP );
-	powerOn(POWER_SOUND);
+    if (module == NULL)
+        XM7_StopModule();
+    else
+        XM7_PlayModule(module);
+}
 
-	readUserSettings();
-	ledBlink(0);
+int main(int argc, char *argv[])
+{
+    // clear sound registers
+    dmaFillWords(0, (void *)0x04000400, 0x100);
 
-	irqInit();
-	// Start the RTC tracking IRQ
-	initClockIRQ();
-	fifoInit();
-	touchInit();
+    REG_SOUNDCNT |= SOUND_ENABLE;
+    writePowerManagement(PM_CONTROL_REG,
+                         (readPowerManagement(PM_CONTROL_REG) & ~PM_SOUND_MUTE ) | PM_SOUND_AMP);
+    powerOn(POWER_SOUND);
 
-	SetYtrigger(80);
+    readUserSettings();
+    ledBlink(0);
 
-	installWifiFIFO();
-	installSoundFIFO();
+    irqInit();
+    // Start the RTC tracking IRQ
+    initClockIRQ();
+    fifoInit();
+    touchInit();
 
-	// Initialize libxm7
-	XM7_Initialize();
-	// Setup the FIFO handler for libXM7
-	fifoSetValue32Handler(FIFO_XM7, XM7_Value32Handler, 0);
+    SetYtrigger(80);
 
-	installSystemFIFO();
+    installWifiFIFO();
+    installSoundFIFO();
 
-	irqSet(IRQ_VCOUNT, VcountHandler);
-	irqSet(IRQ_VBLANK, VblankHandler);
+    // Initialize libxm7
+    XM7_Initialize();
+    // Setup the FIFO handler for libXM7
+    fifoSetValue32Handler(FIFO_XM7, XM7_Value32Handler, 0);
 
-	irqEnable( IRQ_VBLANK | IRQ_VCOUNT | IRQ_NETWORK);
+    installSystemFIFO();
 
-	setPowerButtonCB(powerButtonCB);
+    irqSet(IRQ_VCOUNT, VcountHandler);
+    irqSet(IRQ_VBLANK, VblankHandler);
 
-	// Keep the ARM7 mostly idle
-	while (!exitflag) {
-		if ( 0 == (REG_KEYINPUT & (KEY_SELECT | KEY_START | KEY_L | KEY_R))) {
-			exitflag = true;
-		}
-		swiWaitForVBlank();
-	}
-	return 0;
+    irqEnable(IRQ_VBLANK | IRQ_VCOUNT | IRQ_NETWORK);
+
+    setPowerButtonCB(powerButtonCB);
+
+    // Keep the ARM7 mostly idle
+    while (!exitflag)
+    {
+        if (0 == (REG_KEYINPUT & (KEY_SELECT | KEY_START | KEY_L | KEY_R)))
+            exitflag = true;
+
+        swiWaitForVBlank();
+    }
+
+    return 0;
 }
