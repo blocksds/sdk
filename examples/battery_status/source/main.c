204f51e62908f0717e66f14c06293e38d213d10f
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-10-13 02:10:31 +0100

    examples: Add example to get the battery level

---
+++ examples/battery_status/source/main.c
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: CC0-1.0
+//
+// SPDX-FileContributor: Antonio Niño Díaz, 2023
+
+#include <stdio.h>
+
+#include <nds.h>
+
+int main(int argc, char **argv)
+{
+    consoleDemoInit();
+
+    while (1)
+    {
+        swiWaitForVBlank();
+
+        uint32_t value = getBatteryLevel();
+        uint32_t battery_level = value & 0xF;
+        uint32_t charger_connected = value & BIT(7);
+
+        // Clear console
+        printf("\x1b[2J");
+        printf("Current charge level: %" PRIu32 "\n", battery_level);
+        printf("Charger connected: %s\n", charger_connected ? "Yes" : "No");
+        printf("\n");
+        printf("Press START to exit");
+
+        scanKeys();
+
+        if (keysDown() & KEY_START)
+            break;
+    }
+
+    return 0;
+}
