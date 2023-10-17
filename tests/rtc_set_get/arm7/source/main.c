12fe8d8bf5c4533610dc39eed2c476b8161defac
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-10-17 03:08:57 +0100

    examples: Add example of using the RTC functions

---
+++ tests/rtc_set_get/arm7/source/main.c
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: CC0-1.0
+//
+// SPDX-FileContributor: Antonio Niño Díaz, 2023
+
+#include <stdio.h>
+#include <time.h>
+
+#include <nds.h>
+
+volatile bool exit_loop = false;
+
+void power_button_callback(void)
+{
+    exit_loop = true;
+}
+
+void vblank_handler(void)
+{
+    inputGetAndSend();
+}
+
+void fifo_handler_datamsg_time_date(int num_bytes, void *userdata)
+{
+    rtcTimeAndDate rtc_time_date;
+    fifoGetDatamsg(FIFO_USER_01, sizeof(rtc_time_date), (void *)&rtc_time_date);
+
+    uint32_t ok = 0;
+
+    if (rtcTimeAndDateSet(&rtc_time_date) == 0)
+        ok = 1;
+
+    // Read the RTC to get the new date instead of assuming the write succeeded
+    resyncClock();
+
+    fifoSendValue32(FIFO_USER_01, ok);
+}
+
+void fifo_handler_datamsg_time(int num_bytes, void *userdata)
+{
+    rtcTime rtc_time;
+    fifoGetDatamsg(FIFO_USER_02, sizeof(rtc_time), (void *)&rtc_time);
+
+    uint32_t ok = 0;
+
+    if (rtcTimeSet(&rtc_time) == 0)
+        ok = 1;
+
+    // Read the RTC to get the new date instead of assuming the write succeeded
+    resyncClock();
+
+    fifoSendValue32(FIFO_USER_01, ok);
+}
+
+int main(int argc, char *argv[])
+{
+    // Initialize sound hardware
+    enableSound();
+
+    // Read user information from the firmware (name, birthday, etc)
+    readUserSettings();
+
+    // Stop LED blinking
+    ledBlink(0);
+
+    // Using the calibration values read from the firmware with
+    // readUserSettings(), calculate some internal values to convert raw
+    // coordinates into screen coordinates.
+    touchInit();
+
+    irqInit();
+    irqSet(IRQ_VBLANK, vblank_handler);
+
+    fifoInit();
+
+    installSoundFIFO();
+    installSystemFIFO(); // Sleep mode, storage, firmware...
+
+    // This sets a callback that is called when the power button in a DSi
+    // console is pressed. It has no effect in a DS.
+    setPowerButtonCB(power_button_callback);
+
+    // Read current date from the RTC and setup an interrupt to update the time
+    // regularly. The interrupt simply adds one second every time, it doesn't
+    // read the date. Reading the RTC is very slow, so it's a bad idea to do it
+    // frequently.
+    initClockIRQTimer(3);
+
+    irqEnable(IRQ_VBLANK);
+
+    // This channel will listen to messages from the ARM9 with a time and date
+    fifoSetDatamsgHandler(FIFO_USER_01, fifo_handler_datamsg_time_date, NULL);
+    // This one will only change the time
+    fifoSetDatamsgHandler(FIFO_USER_02, fifo_handler_datamsg_time, NULL);
+
+    while (!exit_loop)
+    {
+        const uint16_t key_mask = KEY_SELECT | KEY_START | KEY_L | KEY_R;
+        uint16_t keys_pressed = ~REG_KEYINPUT;
+
+        if ((keys_pressed & key_mask) == key_mask)
+            exit_loop = true;
+
+        swiWaitForVBlank();
+    }
+
+    return 0;
+}
---
+++ tests/rtc_set_get/arm9/source/main.c
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: CC0-1.0
+//
+// SPDX-FileContributor: Antonio Niño Díaz, 2023
+
+// This example shows how to set the time of the real time clock of the DS.
+
+#include <stdio.h>
+#include <time.h>
+
+#include <nds.h>
+
+volatile uint32_t ok = 0;
+
+void FIFO_UserValue32Handler(u32 value, void *userdata)
+{
+    ok = value;
+}
+
+typedef enum {
+    s_year, s_month, s_day, s_hour, s_min, s_sec
+} selection_t;
+
+int main(int argc, char **argv)
+{
+    consoleDemoInit();
+
+    rtcTimeAndDate rtc_time_and_date;
+
+    rtc_time_and_date.year = 2023 - 2000;
+    rtc_time_and_date.month = 11;
+    rtc_time_and_date.day = 17;
+    rtc_time_and_date.hours = 21;
+    rtc_time_and_date.minutes = 35;
+    rtc_time_and_date.seconds = 12;
+
+    selection_t sel = s_year;
+
+    keysSetRepeat(20, 5);
+
+    // Setup the FIFO handler
+    fifoSetValue32Handler(FIFO_USER_01, FIFO_UserValue32Handler, 0);
+
+    while (1)
+    {
+        swiWaitForVBlank();
+
+        printf("\x1b[2J"); // Clear console
+
+        // Print current time
+        char str[100];
+        time_t t = time(NULL);
+        struct tm *tmp = localtime(&t);
+        printf("Current time:\n\n");
+        if (strftime(str, sizeof(str), "%Y-%m-%dT%H:%M:%S%z", tmp) == 0)
+            snprintf(str, sizeof(str), "Failed to get time");
+        printf("%s\n\n", str);
+
+        // Print time adjustment interface
+        const int coord_x[6] = {
+            3, 6, 9, 12, 15, 18
+        };
+        printf("New time:\n\n");
+        printf("\x1b[%dC^\n", coord_x[sel]);
+        printf("%02u-%02u-%02u %02u:%02u:%02u\n",
+               rtc_time_and_date.year + 2000, rtc_time_and_date.month,
+               rtc_time_and_date.day, rtc_time_and_date.hours,
+               rtc_time_and_date.minutes, rtc_time_and_date.seconds);
+        printf("\x1b[%dCv\n", coord_x[sel]);
+        printf("\n");
+
+        // Print general information
+        printf("A: Set time+date\n");
+        printf("B: Set time\n");
+        printf("Pad: Adjust time\n");
+        printf("\n");
+        printf("Last write ok = %lu\n", ok);
+        printf("\n");
+        printf("Press START to exit");
+
+        scanKeys();
+
+        uint16_t keys = keysDownRepeat();
+
+        if (keys & KEY_A)
+        {
+            fifoSendDatamsg(FIFO_USER_01, sizeof(rtc_time_and_date),
+                            (void *)&rtc_time_and_date);
+        }
+
+        if (keys & KEY_B)
+        {
+            rtcTime rtc_time = {
+                rtc_time_and_date.hours,
+                rtc_time_and_date.minutes,
+                rtc_time_and_date.seconds,
+            };
+
+            fifoSendDatamsg(FIFO_USER_02, sizeof(rtc_time), (void *)&rtc_time);
+        }
+
+        if (keys & KEY_LEFT)
+        {
+            if (sel > s_year)
+                sel--;
+        }
+
+        if (keys & KEY_RIGHT)
+        {
+            if (sel < s_sec)
+                sel++;
+        }
+
+        if (keys & KEY_DOWN)
+        {
+            switch (sel)
+            {
+                case s_year:
+                    rtc_time_and_date.year--;
+                    break;
+                case s_month:
+                    rtc_time_and_date.month--;
+                    break;
+                case s_day:
+                    rtc_time_and_date.day--;
+                    break;
+                case s_hour:
+                    rtc_time_and_date.hours--;
+                    break;
+                case s_min:
+                    rtc_time_and_date.minutes--;
+                    break;
+                case s_sec:
+                    rtc_time_and_date.seconds--;
+                    break;
+            }
+        }
+
+        if (keys & KEY_UP)
+        {
+            switch (sel)
+            {
+                case s_year:
+                    rtc_time_and_date.year++;
+                    break;
+                case s_month:
+                    rtc_time_and_date.month++;
+                    break;
+                case s_day:
+                    rtc_time_and_date.day++;
+                    break;
+                case s_hour:
+                    rtc_time_and_date.hours++;
+                    break;
+                case s_min:
+                    rtc_time_and_date.minutes++;
+                    break;
+                case s_sec:
+                    rtc_time_and_date.seconds++;
+                    break;
+            }
+        }
+
+        if (keys & KEY_START)
+            break;
+    }
+
+    return 0;
+}
