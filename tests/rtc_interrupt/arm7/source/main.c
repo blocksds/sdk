b8494bbc2802804c6df05e00f9270c097d49374e
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-10-18 20:35:33 +0100

    tests: Add test for the RTC interrupt
    Now that it has been deprecated I'd like to have some way to ensure that
    it doesn't completely stop working over time.

---
+++ tests/rtc_interrupt/arm7/source/main.c
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: Zlib
+//
+// Copyright (C) 2005 Michael Noland (joat)
+// Copyright (C) 2005 Jason Rogers (Dovoto)
+// Copyright (C) 2005-2015 Dave Murphy (WinterMute)
+// Copyright (C) 2023 Antonio Niño Díaz
+
+// ARM7 core that uses the RTC interrupt instead of using a timer interrupt to
+// update the time every second.
+
+#include <dswifi7.h>
+#include <nds.h>
+#include <maxmod7.h>
+
+volatile bool exit_loop = false;
+
+void power_button_callback(void)
+{
+    exit_loop = true;
+}
+
+void vblank_handler(void)
+{
+    inputGetAndSend();
+    Wifi_Update();
+}
+
+int main(int argc, char *argv[])
+{
+    // Initialize sound hardware
+    enableSound();
+
+    // Read user information from the firmware (name, birthday, etc)
+    readUserSettings();
+
+    // Stop LED blinking
+    ledBlink(0);
+
+    // Using the calibration values read from the firmware with
+    // readUserSettings(), calculate some internal values to convert raw
+    // coordinates into screen coordinates.
+    touchInit();
+
+    irqInit();
+    irqSet(IRQ_VBLANK, vblank_handler);
+
+    fifoInit();
+
+    installWifiFIFO();
+    installSoundFIFO();
+    installSystemFIFO(); // Sleep mode, storage, firmware...
+
+    // Initialize Maxmod. It uses timer 0 internally.
+    mmInstall(FIFO_MAXMOD);
+
+    // This sets a callback that is called when the power button in a DSi
+    // console is pressed. It has no effect in a DS.
+    setPowerButtonCB(power_button_callback);
+
+    // Read current date from the RTC and setup an interrupt to update the time
+    // regularly. The interrupt simply adds one second every time, it doesn't
+    // read the date. Reading the RTC is very slow, so it's a bad idea to do it
+    // frequently. This function only sets up the RTC interrupt, it doesn't
+    // enable it.
+    //
+    // Also, note that it has been deprecated. The pragmas here are used to
+    // silence the warning that GCC generates when using the function.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+    initClockIRQ();
+#pragma GCC diagnostic pop
+
+    // Enable the RTC interrupt
+    irqEnable(IRQ_VBLANK | IRQ_RTC);
+
+    while (!exit_loop)
+    {
+        const uint16_t key_mask = KEY_SELECT | KEY_START | KEY_L | KEY_R;
+        uint16_t keys_pressed = ~REG_KEYINPUT;
+
+        if ((keys_pressed & key_mask) == key_mask)
+            exit_loop = true;
+
+        swiWaitForVBlank();
+    }
+
+    return 0;
+}
---
+++ tests/rtc_interrupt/arm9/source/main.c
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: CC0-1.0
+//
+// SPDX-FileContributor: Antonio Niño Díaz, 2023
+
+// Using the RTC interrupt has been deprecated in libnds because it's not a
+// reliable way to keep track of the time. It doesn't work on the 3DS in DS/DSi
+// mode, and it doesn't work on most emulators.
+//
+// However, it may still be useful in the future to keep it working. This test
+// has been added so that we can verify it doesn't completely break over time.
+
+#include <stdio.h>
+#include <time.h>
+
+#include <nds.h>
+
+int main(int argc, char **argv)
+{
+    // Setup sub screen for the text console
+    consoleDemoInit();
+
+    while (1)
+    {
+        // Synchronize game loop to the screen refresh
+        swiWaitForVBlank();
+
+        // Clear console
+        printf("\x1b[2J");
+
+        // Print current time
+        char str[100];
+        time_t t = time(NULL);
+        struct tm *tmp = localtime(&t);
+        if (strftime(str, sizeof(str), "%Y-%m-%dT%H:%M:%S%z", tmp) == 0)
+            snprintf(str, sizeof(str), "Failed to get time");
+        printf("%s\n", str);
+
+        printf("\n"
+               "\n"
+               "\n"
+               "\n"
+               "The RTC interrupt should make\n"
+               "the time change every second.\n"
+               "\n"
+               "The 3DS doesn't support the RTC\n"
+               "interrupt, so it won't change.\n"
+               "\n"
+               "Only no$gba supports it out of\n"
+               "the emulators I've tested.\n"
+               "\n"
+               "\n"
+               "\n"
+               "\n");
+
+        // Print some controls
+        printf("START:  Exit to loader\n");
+
+        scanKeys();
+
+        uint16_t keys = keysHeld();
+
+        if (keys & KEY_START)
+            break;
+    }
+
+    return 0;
+}
