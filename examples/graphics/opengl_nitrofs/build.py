a6cd39a703c58154d99c55f292ee47b2068f26d3
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-02-15 18:07:39 +0000

    examples: Add example of loading textures from NitroFS

---
+++ examples/graphics/opengl_nitrofs/build.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# SPDX-License-Identifier: CC0-1.0
+#
+# SPDX-FileContributor: Antonio Niño Díaz, 2024
+
+from architectds import *
+
+nitrofs = NitroFS()
+nitrofs.add_grit(['graphics'])
+nitrofs.generate_image()
+
+arm9 = Arm9Binary(
+    sourcedirs=['source'],
+)
+arm9.generate_elf()
+
+nds = NdsRom(
+    binaries=[arm9, nitrofs],
+    game_title='OpenGL + NitroFS',
+)
+nds.generate_nds()
+
+nds.run_command_line_arguments()
---
+++ examples/graphics/opengl_nitrofs/source/main.c
@@ -0,0 +1,176 @@
+// SPDX-License-Identifier: CC0-1.0
+//
+// SPDX-FileContributor: Antonio Niño Díaz, 2023
+
+#include <stdio.h>
+
+#include <filesystem.h>
+#include <nds.h>
+
+__attribute__((noreturn)) void wait_forever(void)
+{
+    while (1)
+    {
+        swiWaitForVBlank();
+
+        scanKeys();
+        if (keysHeld() & KEY_START)
+            exit(1);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int textureID;
+
+    // Setup sub screen for the text console
+    consoleDemoInit();
+
+    printf("Initializing NitroFS...\n");
+
+    if (!nitroFSInit(NULL))
+    {
+        perror("nitroFSInit()");
+        wait_forever();
+    }
+
+    videoSetMode(MODE_0_3D);
+
+    glInit();
+
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_ANTIALIAS);
+
+    // The background must be fully opaque and have a unique polygon ID
+    // (different from the polygons that are going to be drawn) so that
+    // antialias works.
+    glClearColor(0, 0, 0, 31);
+    glClearPolyID(63);
+
+    glClearDepth(0x7FFF);
+
+    glViewport(0, 0, 255, 191);
+
+    // Setup some VRAM as memory for textures
+    vramSetBankA(VRAM_A_TEXTURE);
+
+    void *gfxDst = NULL;
+    GRFHeader header = { 0 };
+    GRFError err = grfLoadPath("grit/neon_png.grf", &header, &gfxDst, NULL,
+                               NULL, NULL, NULL, NULL);
+    if (err != GRF_NO_ERROR)
+    {
+        printf("Couldn't load GRF file: %d", err);
+        wait_forever();
+    }
+
+    if (gfxDst == NULL)
+    {
+        printf("No graphics found in GRF file");
+        wait_forever();
+    }
+
+    if (header.gfxAttr != 16)
+    {
+        printf("Invalid format in GRF file");
+        wait_forever();
+    }
+
+    if ((header.gfxWidth != 128) || (header.gfxHeight != 128))
+    {
+        printf("Texture size needs to be 128x128");
+        wait_forever();
+    }
+
+    // Load texture
+    glGenTextures(1, &textureID);
+    glBindTexture(0, textureID);
+    glTexImage2D(0, 0, GL_RGB, TEXTURE_SIZE_128 , TEXTURE_SIZE_128, 0,
+                 TEXGEN_TEXCOORD, gfxDst);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(70, 256.0 / 192.0, 0.1, 40);
+
+    gluLookAt(0.0, 0.0, 2.0,  // Position
+              0.0, 0.0, 0.0,  // Look at
+              0.0, 1.0, 0.0); // Up
+
+    int angle_x = 0;
+    int angle_z = 0;
+
+    while (1)
+    {
+        // Synchronize game loop to the screen refresh
+        swiWaitForVBlank();
+
+        // Print some text in the demo console
+        // -----------------------------------
+
+        // Clear console
+        printf("\x1b[2J");
+
+        // Print some controls
+        printf("PAD:    Rotate triangle\n");
+        printf("START:  Exit to loader\n");
+        printf("\n");
+
+        // Handle user input
+        // -----------------
+
+        scanKeys();
+
+        uint16_t keys = keysHeld();
+
+        if (keys & KEY_LEFT)
+            angle_z += 3;
+        if (keys & KEY_RIGHT)
+            angle_z -= 3;
+
+        if (keys & KEY_UP)
+            angle_x += 3;
+        if (keys & KEY_DOWN)
+            angle_x -= 3;
+
+        if (keys & KEY_START)
+            break;
+
+        // Render 3D scene
+        // ---------------
+
+        glPushMatrix();
+
+        glRotateZ(angle_z);
+        glRotateX(angle_x);
+
+        glMatrixMode(GL_MODELVIEW);
+
+        glPolyFmt(POLY_ALPHA(31) | POLY_CULL_NONE);
+
+        glBindTexture(0, textureID);
+
+        glColor3f(1, 1, 1);
+
+        glBegin(GL_QUADS);
+
+            GFX_TEX_COORD = (TEXTURE_PACK(0, inttot16(128)));
+            glVertex3v16(floattov16(-1), floattov16(-1), 0);
+
+            GFX_TEX_COORD = (TEXTURE_PACK(inttot16(128),inttot16(128)));
+            glVertex3v16(floattov16(1), floattov16(-1), 0);
+
+            GFX_TEX_COORD = (TEXTURE_PACK(inttot16(128), 0));
+            glVertex3v16(floattov16(1), floattov16(1), 0);
+
+            GFX_TEX_COORD = (TEXTURE_PACK(0,0));
+            glVertex3v16(floattov16(-1), floattov16(1), 0);
+
+        glEnd();
+
+        glPopMatrix(1);
+
+        glFlush(0);
+    }
+
+    return 0;
+}
