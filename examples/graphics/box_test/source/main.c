97b0f08bc8c732a7f1b4954f301fc59002d1e0fd
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-02-16 02:04:31 +0000

    examples: Add box test example
    This doesn't seem particularly reliable when the box is only partially
    inside the screen (tested on hardware).

---
+++ examples/graphics/box_test/source/main.c
@@ -0,0 +1,179 @@
+// SPDX-License-Identifier: CC0-1.0
+//
+// SPDX-FileContributor: Antonio Niño Díaz, 2024
+
+#include <stdio.h>
+
+#include <nds.h>
+
+void draw_box(float bx_, float by_, float bz_, float ex_, float ey_, float ez_)
+{
+    // Begin and end coordinates
+    int bx = floattov16(bx_);
+    int ex = floattov16(ex_);
+    int by = floattov16(by_);
+    int ey = floattov16(ey_);
+    int bz = floattov16(bz_);
+    int ez = floattov16(ez_);
+
+    glBegin(GL_QUADS);
+
+        glColor3f(1, 0, 0);
+
+        glVertex3v16(bx, by, bz);
+        glVertex3v16(bx, ey, bz);
+        glVertex3v16(ex, ey, bz);
+        glVertex3v16(ex, by, bz);
+
+        glColor3f(0, 1, 0);
+
+        glVertex3v16(bx, by, ez);
+        glVertex3v16(bx, ey, ez);
+        glVertex3v16(ex, ey, ez);
+        glVertex3v16(ex, by, ez);
+
+        glColor3f(0, 0, 1);
+
+        glVertex3v16(bx, by, bz);
+        glVertex3v16(bx, by, ez);
+        glVertex3v16(ex, by, ez);
+        glVertex3v16(ex, by, bz);
+
+        glColor3f(1, 0, 1);
+
+        glVertex3v16(bx, ey, bz);
+        glVertex3v16(bx, ey, ez);
+        glVertex3v16(ex, ey, ez);
+        glVertex3v16(ex, ey, bz);
+
+        glColor3f(0, 1, 1);
+
+        glVertex3v16(bx, by, bz);
+        glVertex3v16(bx, by, ez);
+        glVertex3v16(bx, ey, ez);
+        glVertex3v16(bx, ey, bz);
+
+        glColor3f(1, 1, 0);
+
+        glVertex3v16(ex, by, bz);
+        glVertex3v16(ex, by, ez);
+        glVertex3v16(ex, ey, ez);
+        glVertex3v16(ex, ey, bz);
+
+    glEnd();
+}
+
+int main(int argc, char **argv)
+{
+    // Setup sub screen for the text console
+    consoleDemoInit();
+
+    videoSetMode(MODE_0_3D);
+
+    glInit();
+
+    glEnable(GL_ANTIALIAS);
+
+    // The background must be fully opaque and have a unique polygon ID
+    // (different from the polygons that are going to be drawn) so that
+    // antialias works.
+    glClearColor(0, 0, 0, 31);
+    glClearPolyID(63);
+
+    glClearDepth(0x7FFF);
+
+    glViewport(0, 0, 255, 191);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(70, 256.0 / 192.0, 0.1, 40);
+
+    gluLookAt(0.0, 0.0, 2.0,  // Position
+              0.0, 0.0, 0.0,  // Look at
+              0.0, 1.0, 0.0); // Up
+
+    int angle_x = 0;
+    int angle_z = 0;
+
+    float x = 0.0;
+    float y = 0.0;
+    float z = 0.0;
+
+    while (1)
+    {
+        // Synchronize game loop to the screen refresh
+        swiWaitForVBlank();
+
+        // Print some text in the demo console
+        // -----------------------------------
+
+        // Clear console
+        printf("\x1b[2J");
+
+        // Print some controls
+        printf("PAD:     Move\n");
+        printf("A,B,X,Y: Rotate\n");
+        printf("START:   Exit to loader\n");
+        printf("\n");
+        printf("This test seems to be unreliable.\n");
+        printf("\n");
+
+        // Handle user input
+        // -----------------
+
+        scanKeys();
+
+        uint16_t keys = keysHeld();
+
+        if (keys & KEY_LEFT)
+            x -= 0.1;
+        if (keys & KEY_RIGHT)
+            x += 0.1;
+
+        if (keys & KEY_UP)
+            y += 0.1;
+        if (keys & KEY_DOWN)
+            y -= 0.1;
+
+        if (keys & KEY_A)
+            angle_x += 3;
+        if (keys & KEY_Y)
+            angle_x -= 3;
+
+        if (keys & KEY_X)
+            angle_z += 3;
+        if (keys & KEY_B)
+            angle_z -= 3;
+
+        if (keys & KEY_START)
+            break;
+
+        // Render 3D scene
+        // ---------------
+
+        glMatrixMode(GL_MODELVIEW);
+
+        glPushMatrix();
+
+        glTranslatef(x, y, z);
+
+        glRotateY(angle_z);
+        glRotateX(angle_x);
+
+        int in = BoxTestf(-0.5, -0.5, -0.5,
+                          0.5, 0.5, 0.5);
+
+        printf("Is box on screen? %s", in ? "Yes" : "No");
+
+        glPolyFmt(POLY_ALPHA(31) | POLY_CULL_NONE);
+
+        draw_box(-0.5, -0.5, -0.5,
+                 0.5, 0.5, 0.5);
+
+        glPopMatrix(1);
+
+        glFlush(0);
+    }
+
+    return 0;
+}
