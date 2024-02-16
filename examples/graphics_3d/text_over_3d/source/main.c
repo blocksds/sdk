68827cb2738933ded2d16c720d7151a9a1faea7f
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-02-16 15:01:53 +0000

    examples: Add example of printing text over 3D graphics

---
+++ examples/graphics_3d/text_over_3d/source/main.c
@@ -0,0 +1,165 @@
+// SPDX-License-Identifier: CC0-1.0
+//
+// SPDX-FileContributor: Antonio Niño Díaz, 2024
+
+#include <stdio.h>
+
+#include <nds.h>
+
+void draw_box(float bx_, float by_, float bz_, float ex_, float ey_, float ez_)
+{
+    // Begin and end coordinates
+    int bx = floattov16(bx_);
+    int ex = floattov16(ex_);
+    int by = floattov16(by_);
+    int ey = floattov16(ey_);
+    int bz = floattov16(bz_);
+    int ez = floattov16(ez_);
+
+    glBegin(GL_QUADS);
+
+        glColor3f(1, 0, 0);
+
+        glVertex3v16(bx, by, bz);
+        glVertex3v16(bx, ey, bz);
+        glVertex3v16(ex, ey, bz);
+        glVertex3v16(ex, by, bz);
+
+        glColor3f(0, 1, 0);
+
+        glVertex3v16(bx, by, ez);
+        glVertex3v16(bx, ey, ez);
+        glVertex3v16(ex, ey, ez);
+        glVertex3v16(ex, by, ez);
+
+        glColor3f(0, 0, 1);
+
+        glVertex3v16(bx, by, bz);
+        glVertex3v16(bx, by, ez);
+        glVertex3v16(ex, by, ez);
+        glVertex3v16(ex, by, bz);
+
+        glColor3f(1, 0, 1);
+
+        glVertex3v16(bx, ey, bz);
+        glVertex3v16(bx, ey, ez);
+        glVertex3v16(ex, ey, ez);
+        glVertex3v16(ex, ey, bz);
+
+        glColor3f(0, 1, 1);
+
+        glVertex3v16(bx, by, bz);
+        glVertex3v16(bx, by, ez);
+        glVertex3v16(bx, ey, ez);
+        glVertex3v16(bx, ey, bz);
+
+        glColor3f(1, 1, 0);
+
+        glVertex3v16(ex, by, bz);
+        glVertex3v16(ex, by, ez);
+        glVertex3v16(ex, ey, ez);
+        glVertex3v16(ex, ey, bz);
+
+    glEnd();
+}
+
+int main(int argc, char **argv)
+{
+    videoSetMode(MODE_0_3D);
+
+    // Allocate some VRAM to be used for backgrounds in the main engine
+    vramSetBankH(VRAM_F_MAIN_BG_0x06000000);
+
+    // Setup layer 1 as console layer
+    consoleInit(NULL, 1, BgType_Text4bpp, BgSize_T_256x256, 7, 0, true, true);
+
+    // Set layer 0 (3D layer) to priority 1 (lower priority than 0)
+    bgSetPriority(0, 1);
+
+    // Set layer 1 (console) to priority 0
+    bgSetPriority(1, 0);
+
+    glInit();
+
+    glEnable(GL_ANTIALIAS);
+
+    // The background must be fully opaque and have a unique polygon ID
+    // (different from the polygons that are going to be drawn) so that
+    // antialias works.
+    glClearColor(0, 0, 0, 31);
+    glClearPolyID(63);
+
+    glClearDepth(0x7FFF);
+
+    glViewport(0, 0, 255, 191);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(70, 256.0 / 192.0, 0.1, 40);
+
+    gluLookAt(0.0, 0.0, 1.5,  // Position
+              0.0, 0.0, 0.0,  // Look at
+              0.0, 1.0, 0.0); // Up
+
+    int angle_x = 45;
+    int angle_z = 45;
+
+    while (1)
+    {
+        // Synchronize game loop to the screen refresh
+        swiWaitForVBlank();
+
+        // Print some text in the demo console
+        // -----------------------------------
+
+        // Clear console
+        printf("\x1b[2J");
+
+        printf("Text console over 3D\n");
+        printf("\n");
+        printf("Pad:   Rotate\n");
+        printf("\n");
+        printf("START: Exit to loader\n");
+
+        // Handle user input
+        // -----------------
+
+        scanKeys();
+
+        uint16_t keys = keysHeld();
+
+        if (keys & KEY_LEFT)
+            angle_x += 3;
+        if (keys & KEY_RIGHT)
+            angle_x -= 3;
+
+        if (keys & KEY_UP)
+            angle_z += 3;
+        if (keys & KEY_DOWN)
+            angle_z -= 3;
+
+        if (keys & KEY_START)
+            break;
+
+        // Render 3D scene
+        // ---------------
+
+        glMatrixMode(GL_MODELVIEW);
+
+        glPushMatrix();
+
+        glRotateY(angle_z);
+        glRotateX(angle_x);
+
+        glPolyFmt(POLY_ALPHA(31) | POLY_CULL_NONE);
+
+        draw_box(-0.5, -0.5, -0.5,
+                 0.5, 0.5, 0.5);
+
+        glPopMatrix(1);
+
+        glFlush(0);
+    }
+
+    return 0;
+}
