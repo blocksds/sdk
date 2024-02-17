188b78a2c1a9dea9483b8edb4cf58cfcadf8c299
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-02-17 12:44:36 +0000

    examples: Add example of printing text to both screens
    This is already used in some examples like the asynch_loading one, but it's
    more discoverable this way.

---
+++ examples/console/both_screens/source/main.c
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: CC0-1.0
+//
+// SPDX-FileContributor: Antonio Niño Díaz, 2024
+
+#include <stdio.h>
+
+#include <nds.h>
+
+int main(int argc, char **argv)
+{
+    PrintConsole topScreen;
+    PrintConsole bottomScreen;
+
+    videoSetMode(MODE_0_2D);
+    videoSetModeSub(MODE_0_2D);
+
+    vramSetBankA(VRAM_A_MAIN_BG);
+    vramSetBankC(VRAM_C_SUB_BG);
+
+    consoleInit(&topScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 31, 0, true, true);
+    consoleInit(&bottomScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 31, 0, false, true);
+
+    consoleSelect(&bottomScreen);
+
+    printf("\x1b[2J"); // Clear console
+    printf("Printing on the bottom screen\n");
+
+    consoleSelect(&topScreen);
+
+    printf("Printing on the top screen\n");
+    printf("\n");
+    printf("\n");
+    printf("Press START to exit to loader\n");
+
+    while (1)
+    {
+        swiWaitForVBlank();
+
+        scanKeys();
+
+        if (keysHeld() & KEY_START)
+            break;
+    }
+
+    return 0;
+}
