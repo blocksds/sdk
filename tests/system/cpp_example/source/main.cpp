6f0f5d2c1735753a2bcff118da98db7180858d91
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-03-27 01:43:15 +0000

    tests: Improve C++ test
    Test OS-related systems like memory allocation and filesystem access.

--- tests/system/cpp_example/source/main.cpp
+++ tests/system/cpp_example/source/main.cpp
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: CC0-1.0
 //
-// SPDX-FileContributor: Antonio Niño Díaz, 2023
+// SPDX-FileContributor: Antonio Niño Díaz, 2023-2024
 
 // This example uses a few C++ features that hopefully test whether the
 // linkerscript and crt0 setup the C++ runtime correctly.
 
 #include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <string>
 #include <string>
 #include <vector>
 
+#include <filesystem.h>
 #include <nds.h>
 
 // ------------------------------------------------------------------
@@ -112,7 +116,7 @@ void vector_test(void)
     {
         printf("%i ", i);
     }
-    puts("\n");
+    printf("\n");
 }
 
 // ------------------------------------------------------------------
@@ -125,7 +129,106 @@ void string_test(void)
     std::string greeting = "[std::string] " + hello + " " + world + "!";
 
     printf("%s ", greeting.c_str());
-    puts("\n");
+    printf("\n");
+}
+
+// ------------------------------------------------------------------
+
+void new_test(void)
+{
+    float *p = new float[]{1.5, 2, 3.1};
+
+    printf("new: %.2f %.2f %.2f\n", p[0], p[1], p[2]);
+
+    delete[] p;
+}
+
+// ------------------------------------------------------------------
+
+void fcat(const char *path)
+{
+    printf("cat: %s\n", path);
+
+    FILE *f = fopen(path, "r");
+    if (f == NULL)
+    {
+        perror("fopen");
+        return;
+    }
+
+    int ret = fseek(f, 0, SEEK_END);
+    if (ret != 0)
+    {
+        perror("fseek");
+        return;
+    }
+
+    long size = ftell(f);
+    if (size == -1)
+    {
+        perror("ftell");
+        return;
+    }
+
+    printf("Size: %ld bytes\n", size);
+
+    rewind(f);
+
+    char *buffer = (char *)malloc(size + 1);
+    if (buffer == NULL)
+    {
+        printf("malloc(): Not enough memory (%ld)\n", size);
+        return;
+    }
+
+    size_t bytes = fread(buffer, 1, size, f);
+    if (bytes != (size_t)size)
+    {
+        printf("fread(%ld): %zu bytes read", size, bytes);
+        return;
+    }
+    buffer[size - 1] = '\0';
+
+    printf("[%s]\n", buffer);
+
+    int res = fclose(f);
+    if (res != 0)
+    {
+        perror("fclose");
+        return;
+    }
+
+    free(buffer);
+}
+
+void filesystem_test(void)
+{
+    // Using C++ functions
+
+    std::cout << "Opening 'file.txt'...\n";
+
+    std::string line;
+    std::ifstream myfile("file.txt");
+    if (myfile.is_open())
+    {
+        std::cout << "Read file: [";
+
+        while (getline(myfile, line))
+            std::cout << line << '\n';
+        myfile.close();
+
+        std::cout << "]\n";
+    }
+    else
+    {
+        std::cout << "Failed to open";
+    }
+
+    // Using C functions
+
+    std::cout << "Opening 'file.txt'...\n";
+
+    fcat("file.txt");
 }
 
 // ------------------------------------------------------------------
@@ -134,13 +237,25 @@ int main(int argc, char *argv[])
 {
     consoleDemoInit();
 
+    bool init_ok = nitroFSInit(NULL);
+    if (!init_ok)
+    {
+        perror("nitroFSInit()");
+        goto exit;
+    }
+
+    chdir("nitro:/");
+
     // Run some C++ tests
 
     virtual_functions_test();
     global_constructor_test();
     vector_test();
     string_test();
+    new_test();
+    filesystem_test();
 
+exit:
     printf("\n\n");
     printf("START: Exit to loader\n");
 
