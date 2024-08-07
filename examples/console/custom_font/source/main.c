9353fac76cdee2b1591e5b838b98e223320b9da9
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-08-07 19:23:15 +0100

    examples: Improve custom console example
    Now the example loads two custom 4 BPP fonts with palettes on the sub
    screen and one custom 8 BPP font with palette on the main screen.

--- examples/console/custom_font/source/main.c
+++ examples/console/custom_font/source/main.c
@@ -2,40 +2,131 @@
 //
 // SPDX-FileContributor: Antonio Niño Díaz, 2024
 
-// Information about ANSI escape codes:
-//
-//     https://en.wikipedia.org/wiki/ANSI_escape_code#CSI_(Control_Sequence_Introducer)_sequences
-// Check the source code of the console in libnds to check which codes are
-// supported and which ones are not: source/arm9/console.c
-
 #include <stdio.h>
 
 #include <nds.h>
 
-#include <custom_font.h>
+#include <anuvverbubbla_8x8.h>
+#include <charmap_cellphone.h>
+#include <charmap_futuristic.h>
 
-ConsoleFont customFont = {
-    .gfx = custom_fontTiles,
-    .pal = NULL,
-    .numColors = 0,
-    .bpp = 1,
-    .asciiOffset = 0,
-    .numChars = 128,
+// Size of a color in bytes
+const size_t size_color = 2;
+
+// Size of a font character in 4 and 8 BPP modes in bytes
+const size_t size_char_4bpp = (8 * 8) / 2;
+const size_t size_char_8bpp = 8 * 8;
+
+ConsoleFont font_anuvverbubbla= {
+    .gfx = anuvverbubbla_8x8Tiles,
+    .pal = anuvverbubbla_8x8Pal,
+    .numColors = anuvverbubbla_8x8PalLen / size_color,
+    .bpp = 8,
+    .asciiOffset = 32,
+    .numChars = anuvverbubbla_8x8TilesLen / size_char_8bpp,
+};
+
+ConsoleFont font_cellphone = {
+    .gfx = charmap_cellphoneTiles,
+    .pal = charmap_cellphonePal,
+    .numColors = charmap_cellphonePalLen / size_color,
+    .bpp = 4,
+    .asciiOffset = 32,
+    .numChars = charmap_cellphoneTilesLen / size_char_4bpp,
+};
+
+ConsoleFont font_futuristic = {
+    .gfx = charmap_futuristicTiles,
+    .pal = charmap_futuristicPal,
+    .numColors = charmap_futuristicPalLen / size_color,
+    .bpp = 4,
+    .asciiOffset = 32,
+    .numChars = charmap_futuristicTilesLen / size_char_4bpp,
 };
 
 int main(int argc, char **argv)
 {
-    PrintConsole *console = consoleDemoInit();
+    PrintConsole main_console;
 
-    consoleSetFont(console, &customFont);
+    PrintConsole sub_console1;
+    PrintConsole sub_console2;
 
-    // Clear console: [2J
-    printf("\x1b[2J");
+    videoSetMode(MODE_0_2D);
+    vramSetBankA(VRAM_A_MAIN_BG);
 
-    for (int i = 0; i < 256; i++)
-        printf("\x1b[%d;%dH%c", i / 32, i % 32, i);
+    videoSetModeSub(MODE_0_2D);
+    vramSetBankC(VRAM_C_SUB_BG);
 
-    printf("\x1b[23;0HPress START to exit to loader");
+    setBackdropColorSub(RGB15(5, 5, 5));
+
+    // Initialize the consoles with default libnds settings, but don't load
+    // graphics yet so that it doesn't load the default libnds font and
+    // palettes.
+
+    consoleInitEx(&main_console,
+                  1,                // Background layer
+                  BgType_Text8bpp,  // 8 BPP mode
+                  BgSize_T_256x256, // Size of the background layer
+                  0,                // Use map base 0 (they can't be shared)
+                  3,                // Use tile base 3
+                  0,                // Use palette index 0 (256 color palette)
+                  0,                // Start from character 0 of the tile base
+                  true,             // Main screen
+                  false);           // Don't load graphics
+
+    // As both fonts only have the characters from ASCII 32 to 127, which isn't
+    // a lot of tiles, we can load both of them in the same tile slot. The first
+    // font can start at character 0, the second one can start at character 128.
+    // This lets us optimize VRAM usage.
+    consoleInitEx(&sub_console1,
+                  0,                // Background layer
+                  BgType_Text4bpp,  // 4 BPP mode
+                  BgSize_T_256x256, // Size of the background layer
+                  0,                // Use map base 0 (they can't be shared)
+                  3,                // Use tile base 3
+                  4,                // Use palette index 4 (16 color palettes)
+                  0,                // Start from character 0 of the tile base
+                  false,            // Sub screen
+                  false);           // Don't load graphics
+
+    consoleInitEx(&sub_console2,
+                  2,                // Background layer
+                  BgType_Text4bpp,  // 4 BPP mode
+                  BgSize_T_256x256, // Size of the background layer
+                  1,                // Use map base 1 (they can't be shared)
+                  3,                // Use tile base 3
+                  8,                // Use palette index 8 (16 color palettes)
+                  128,              // Start from character 128 of the tile base
+                  false,            // Sub screen
+                  false);           // Don't load graphics
+
+    // Now, load the custom graphics
+
+    consoleSetFont(&main_console, &font_anuvverbubbla);
+
+    consoleSetFont(&sub_console1, &font_cellphone);
+    consoleSetFont(&sub_console2, &font_futuristic);
+
+    // Print some text to test the consoles
+
+    consoleSelect(&main_console);
+    printf("THIS IS A 8 BPP FONT:\n");
+    for (int i = 0; i < 128; i++)
+        printf("%c", i);
+
+    consoleSelect(&sub_console1);
+    printf("This is a 4 BPP font:\n");
+    for (int i = 0; i < 128; i++)
+        printf("%c", i);
+
+    consoleSelect(&sub_console2);
+    consoleSetCursor(&sub_console2, 0, 8);
+    printf("This is a 4 BPP font too:\n");
+    for (int i = 0; i < 128; i++)
+        printf("%c", i);
+
+    consoleSetCursor(&sub_console2, 0, 23);
+    printf("Press START to exit to loader");
 
     while (1)
     {
