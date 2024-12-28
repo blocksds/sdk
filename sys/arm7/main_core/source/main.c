19b6c0e04dac000318a823fe39ee217e9938ecb4
GalaxyShard <dominic.adragna@byteroach.com>
2024-12-28 02:38:12 -0500

    arm7: restructure default_arm7 to allow for more built-in arm7 cores
    Additionally adds build options for enabling/disabling dswifi, maxmod, and
    libxm7 in the main arm7 core

--- sys/arm7/main_core/source/main.c
+++ sys/arm7/main_core/source/main.c
@@ -8,9 +8,39 @@
 
 // Default ARM7 core
 
-#include <dswifi7.h>
 #include <nds.h>
+
+
+#if defined(USE_MAXMOD) && defined(USE_LIBXM7)
+#error "Only one audio library can be used"
+#endif
+
+#if defined(USE_DSWIFI)
+#include <dswifi7.h>
+#endif
+
+#if defined(USE_MAXMOD)
 #include <maxmod7.h>
+#endif
+
+#if defined(USE_LIBXM7)
+#include <libxm7.h>
+
+// Assign FIFO_USER_07 channel to LibXM7
+#define FIFO_LIBXM7 FIFO_USER_07
+
+void XM7_Value32Handler(u32 command, void *userdata)
+{
+    (void)userdata;
+
+    XM7_ModuleManager_Type *module = (XM7_ModuleManager_Type *)command;
+
+    if (module == NULL)
+        XM7_StopModule();
+    else
+        XM7_PlayModule(module);
+}
+#endif
 
 volatile bool exit_loop = false;
 
@@ -22,12 +52,15 @@ void power_button_callback(void)
 void vblank_handler(void)
 {
     inputGetAndSend();
+#if defined(USE_DSWIFI)
     Wifi_Update();
+#endif
 }
 
-int main(int argc, char *argv[])
+int main(void)
 {
     // Initialize sound hardware
+    // Required for some functions within libnds
     enableSound();
 
     // Read user information from the firmware (name, birthday, etc)
@@ -44,14 +77,28 @@ int main(int argc, char *argv[])
     irqInit();
     fifoInit();
 
-    installSoundFIFO();
-    installSystemFIFO(); // Sleep mode, storage, firmware...
+#if defined(USE_DSWIFI)
     installWifiFIFO();
+#endif
+
+    // Required for some functions within libnds
+    installSoundFIFO();
+
+    installSystemFIFO(); // Sleep mode, storage, firmware...
     if (isDSiMode())
         installCameraFIFO();
 
+#if defined(USE_MAXMOD)
     // Initialize Maxmod. It uses timer 0 internally.
     mmInstall(FIFO_MAXMOD);
+#endif
+
+#if defined(USE_LIBXM7)
+    // Initialize LibXM7. It uses timer 0 internally.
+    XM7_Initialize();
+    // Setup the FIFO handler for LibXM7
+    fifoSetValue32Handler(FIFO_LIBXM7, XM7_Value32Handler, 0);
+#endif
 
     // This sets a callback that is called when the power button in a DSi
     // console is pressed. It has no effect in a DS.
