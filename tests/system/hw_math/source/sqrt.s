e3d26a2bb57bc22d20b73f417292da5599fad3ce
Kuratius <Kuratius@gmx.net>
2025-01-24 18:44:49 +0100

    modify capitalization

--- tests/system/hw_math/source/sqrt.s
+++ tests/system/hw_math/source/sqrt.s
@@ -10,17 +10,17 @@
 .cpu     arm946e-s
 .arm
 BEGIN_ASM_FUNC isqrt_asm 
-    MOV    r2, #3 << 30
-    MOV    r1, #1 << 30
+    mov    r2, #3 << 30
+    mov    r1, #1 << 30
     .irp x, \
     0,1,2,3,\
     4,5,6,7,\
     8,9,10,11,\
     12,13,14,15
-    CMP r0, r1, ror #2 * \x
-    SUBHS r0, r0, r1, ror #2 * \x
-    ADC r1, r2, r1, lsl #1
+    cmp r0, r1, ror #2 * \x
+    subhs r0, r0, r1, ror #2 * \x
+    adc r1, r2, r1, lsl #1
     .endr
-    BIC    r0, r1, #3 << 30
-    BX LR
+    bic    r0, r1, #3 << 30
+    bx lr
 
