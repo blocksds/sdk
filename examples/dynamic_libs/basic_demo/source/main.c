4c448c03cb7e0282525cf32a86591639ec72f50c
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-03-22 02:22:06 +0100

    examples: Build dynamic library example with make
    The bash script previously used to build the example has been removed.
    
    Note that this makefile isn't great, the DSL file and NDS ROM are always
    built even if there have been no changes in the library. This happens
    because the top level makefile can't keep track of the dependencies known
    by makefiles in subfolders.

--- examples/dynamic_libs/basic_demo/source/main.c
+++ examples/dynamic_libs/basic_demo/source/main.c
@@ -46,7 +46,7 @@ int main(int argc, char **argv)
 
     printf("Loading library...\n");
 
-    void *h = dlopen("calculator.dsl", RTLD_NOW | RTLD_LOCAL);
+    void *h = dlopen("dsl/calculator.dsl", RTLD_NOW | RTLD_LOCAL);
     const char *err = dlerror();
     if (err != NULL)
     {
