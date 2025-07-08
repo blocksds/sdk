60c40e827a8f6e1262fbf51c3aaf302558df7558
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-07-08 21:10:43 +0100

    examples: Fix example of streaming audio with Maxmod
    Interrupt handlers are supposed to be quick. Sometimes it's unavoidable
    that they take longer, but this is usually done for tasks that take a
    long time to be processed, and the developer can enable nested
    interrupts in that case if required.
    
    Reading files from the filesystem is a very complicated process that can
    involve communicating with the ARM7. This involves the FIFO helpers of
    libnds, threading functions, loops that wait for interrupts, etc. This
    is definitely too much for an interrupt handler.
    
    For example, if the application is running on a DSi, the call stack may
    look like this:
    
      Location | Functions
      ---------+-----------------------------------------------------------
      libnds   | IntrMain() ->
      Maxmod   | mmStreamUpdate() -> StreamExecuteUpdate() ->
      example  | streamingCallback() ->
      picolibc | fread("nitrofs:/...") ->
      libnds   | read() -> nitrofs_read() -> nitrofs_read_internal() ->
      picolibc | fread("sd:/...") ->
      libnds   | read() ->
      FatFs    | f_read() ->
      libnds   | disk_read() -> io->readSectors() / sdmmc_fifo_sectors() ->
      libnds   | fifoWaitValue32Async() -> swiIntrWait()
    
    Doing all this work inside an interrupt handler will use a lot of stack,
    and the problem only gets worse if nested interrupts are enabled.
    `swiIntrWait()` enables interrupts internally, so it was very likely
    that a new interrupt would be handled while waiting for the ARM7 to
    finish reading data.
    
    libnds has recently made a change to prevent `swiIntrWait()` from being
    called if interrupts are disabled [1], but this only fixes the issue of
    nesting interrupts, it doesn't solve the problem of the complexity.
    
    The best thing to avoid this issue is to move the complexity to the main
    loop of the example and use a circular buffer to pass data to the Maxmod
    callback.
    
    [1] https://github.com/blocksds/libnds/commit/a0c2f359184c86661b928a25303954beb7c01a52

--- examples/maxmod/streaming/source/main.c
+++ examples/maxmod/streaming/source/main.c
@@ -1,29 +1,29 @@
 // SPDX-License-Identifier: CC0-1.0
 //
 // SPDX-FileContributor: Michele Di Giorgio, 2024
+// SPDX-FileContributor: Antonio Niño Díaz, 2025
 //
-// Example usage of maxmod streaming. The example loads a WAV file and streams
-// the data through maxmod's utilities.
-//
-// Audio track credits:
-//  https://www.looperman.com/loops/detail/222561/kingmike16-120bpm-techno-synth-loop
+// Example usage of maxmod streaming. The example opens a WAV file and streams
+// the data through maxmod's utilities. The main loop of the application reads
+// the WAV file and saves the data read into a circular buffer. The streaming
+// callback of Maxmod reads from the circular buffer and passes it to Maxmod.
+// This is done because the Maxmod callback runs inside an interrupt handler,
+// and interrupt handlers shouldn't do file reads.
 
 #include <stdio.h>
 
 #include <filesystem.h>
 #include <maxmod9.h>
 #include <nds.h>
-
-#define WAV_FILENAME "nitro:/looperman.wav"
+#include <nds/arm9/dldi.h>
 
 #define DATA_ID 0x61746164
 #define FMT_ID  0x20746d66
 #define RIFF_ID 0x46464952
 #define WAVE_ID 0x45564157
 
-FILE *wavFile = NULL;
-
-typedef struct WAVHeader {
+typedef struct WAVHeader
+{
     // "RIFF" chunk descriptor
     uint32_t chunkID;
     uint32_t chunkSize;
@@ -40,43 +40,134 @@ typedef struct WAVHeader {
     // "data" subchunk
     uint32_t subchunk2ID;
     uint32_t subchunk2Size;
-} WAVHeader_t;
+}
+WAVHeader_t;
+
+#define BUFFER_LENGTH 16384
+
+FILE *wavFile = NULL;
+
+char stream_buffer[BUFFER_LENGTH];
+int stream_buffer_in;
+int stream_buffer_out;
 
 mm_word streamingCallback(mm_word length,
                           mm_addr dest,
                           mm_stream_formats format)
 {
-    // 16-bit, 2 channels (stereo), so we need to output 4 bytes for each word
-    int res = fread(dest, 4, length, wavFile);
+    size_t multiplier = 0;
 
-    if (feof(wavFile)) {
-        // Loop back when song ends
-        fseek(wavFile, sizeof(WAVHeader_t), SEEK_SET);
-        res = fread(dest, 4, length, wavFile);
+    if (format == MM_STREAM_8BIT_MONO)
+        multiplier = 1;
+    else if (format == MM_STREAM_8BIT_STEREO)
+        multiplier = 2;
+    else if (format == MM_STREAM_16BIT_MONO)
+        multiplier = 2;
+    else if (format == MM_STREAM_16BIT_STEREO)
+        multiplier = 4;
+
+    size_t size = length * multiplier;
+
+    size_t bytes_until_end = BUFFER_LENGTH - stream_buffer_out;
+
+    if (bytes_until_end > size)
+    {
+        char *src_ = &stream_buffer[stream_buffer_out];
+
+        memcpy(dest, src_, size);
+        stream_buffer_out += size;
     }
-    // Return the number of words streamed so that if this number is less than
-    // "length", the rest will be buffered and used in the next stream
-    return res;
+    else
+    {
+        char *src_ = &stream_buffer[stream_buffer_out];
+        char *dst_ = dest;
+
+        memcpy(dst_, src_, bytes_until_end);
+        dst_ += bytes_until_end;
+        size -= bytes_until_end;
+
+        src_ = &stream_buffer[0];
+        memcpy(dst_, src_, size);
+        stream_buffer_out = size;
+    }
+
+    return length;
+}
+
+// This reads bytes from wavFile into the provided buffer. If the end of the
+// file is reached, it starts from the start again.
+void readFile(char *buffer, size_t size)
+{
+    while (size > 0)
+    {
+        int res = fread(buffer, 1, size, wavFile);
+        size -= res;
+        buffer += res;
+
+        if (feof(wavFile))
+        {
+            // Loop back when song ends
+            fseek(wavFile, sizeof(WAVHeader_t), SEEK_SET);
+            res = fread(buffer, 1, size, wavFile);
+            size -= res;
+            buffer += res;
+
+            printf("Restarting...\n");
+        }
+    }
+}
+
+void streamingFillBuffer(bool force_fill)
+{
+    if (!force_fill)
+    {
+        if (stream_buffer_in == stream_buffer_out)
+            return;
+    }
+
+    if (stream_buffer_in < stream_buffer_out)
+    {
+        size_t size = stream_buffer_out - stream_buffer_in;
+        readFile(&stream_buffer[stream_buffer_in], size);
+        stream_buffer_in += size;
+    }
+    else
+    {
+        size_t size = BUFFER_LENGTH - stream_buffer_in;
+        readFile(&stream_buffer[stream_buffer_in], size);
+        stream_buffer_in = 0;
+
+        size = stream_buffer_out - stream_buffer_in;
+        readFile(&stream_buffer[stream_buffer_in], size);
+        stream_buffer_in += size;
+    }
+
+    if (stream_buffer_in >= BUFFER_LENGTH)
+        stream_buffer_in -= BUFFER_LENGTH;
 }
 
 int checkWAVHeader(const WAVHeader_t header)
 {
-    if (header.chunkID != RIFF_ID) {
+    if (header.chunkID != RIFF_ID)
+    {
         printf("Wrong RIFF_ID %lx\n", header.chunkID);
         return 1;
     }
 
-    if (header.format != WAVE_ID) {
+    if (header.format != WAVE_ID)
+    {
         printf("Wrong WAVE_ID %lx\n", header.format);
         return 1;
     }
 
-    if (header.subchunk1ID != FMT_ID) {
+    if (header.subchunk1ID != FMT_ID)
+    {
         printf("Wrong FMT_ID %lx\n", header.subchunk1ID);
         return 1;
     }
 
-    if (header.subchunk2ID != DATA_ID) {
+    if (header.subchunk2ID != DATA_ID)
+    {
         printf("Wrong Subchunk2ID %lx\n", header.subchunk2ID);
         return 1;
     }
@@ -86,18 +177,19 @@ int checkWAVHeader(const WAVHeader_t header)
 
 mm_stream_formats getMMStreamType(uint16_t numChannels, uint16_t bitsPerSample)
 {
-    if (numChannels == 1) {
-        if (bitsPerSample == 8) {
+    if (numChannels == 1)
+    {
+        if (bitsPerSample == 8)
             return MM_STREAM_8BIT_MONO;
-        } else {
+        else
             return MM_STREAM_16BIT_MONO;
-        }
-    } else if (numChannels == 2) {
-        if (bitsPerSample == 8) {
+    }
+    else if (numChannels == 2)
+    {
+        if (bitsPerSample == 8)
             return MM_STREAM_8BIT_STEREO;
-        } else {
+        else
             return MM_STREAM_16BIT_STEREO;
-        }
     }
     return MM_STREAM_8BIT_MONO;
 }
@@ -110,58 +202,115 @@ void waitForever(void)
 
 int main(int argc, char **argv)
 {
+    defaultExceptionHandler();
+
     consoleDemoInit();
 
     printf("maxmod streaming example\n");
     printf("===========================\n");
     printf("\n");
+
+    if (!isDSiMode())
+    {
+        printf("Running on NDS.\n");
+        printf("\n");
+        printf("Choose filesystem access mode:\n");
+
+        printf("A: Autodetect\n");
+        printf("L: Force ARM7\n");
+        printf("R: Force ARM9\n");
+
+        while (1)
+        {
+            swiWaitForVBlank();
+
+            scanKeys();
+
+            uint32_t keys_down = keysDown();
+            if (keys_down & KEY_A)
+            {
+                dldiSetMode(DLDI_MODE_AUTODETECT);
+                break;
+            }
+
+            if (keys_down & KEY_L)
+            {
+                dldiSetMode(DLDI_MODE_ARM7);
+                break;
+            }
+
+            if (keys_down & KEY_R)
+            {
+                dldiSetMode(DLDI_MODE_ARM9);
+                break;
+            }
+        }
+
+        printf("\n");
+        printf("DLDI_MODE: %d\n", dldiGetMode());
+        printf("\n");
+    }
+
     printf("START: Return to loader\n");
 
     bool initOK = nitroFSInit(NULL);
-    if (!initOK) {
-        printf("nitroFSInit() error!\n");
+    if (!initOK)
+    {
+        perror("nitroFSInit");
         return 1;
     }
 
-    wavFile = fopen(WAV_FILENAME, "rb");
-    if (wavFile == NULL) {
-        printf("fopen(%s) failed!\n", WAV_FILENAME);
+    wavFile = fopen("nitro:/AuldLangSyne.wav", "rb");
+    if (wavFile == NULL)
+    {
+        perror("fopen");
         waitForever();
     }
 
     WAVHeader_t wavHeader = { 0 };
-    if (fread(&wavHeader, 1, sizeof(WAVHeader_t), wavFile) == 0) {
+    if (fread(&wavHeader, 1, sizeof(WAVHeader_t), wavFile) != sizeof(WAVHeader_t))
+    {
         perror("fread");
         waitForever();
     }
-    if (checkWAVHeader(wavHeader) != 0) {
+    if (checkWAVHeader(wavHeader) != 0)
+    {
         printf("WAV file header is corrupt!\n");
         waitForever();
     }
 
-    // We are not using soundbank so we need to manually initialize
+    // Fill the buffer before we start doing anything
+    streamingFillBuffer(true);
+
+    // We are not using a soundbank so we need to manually initialize
     // mm_ds_system.
-    mm_ds_system mmSys;
-    mmSys.mod_count    = 0;
-    mmSys.samp_count   = 0;
-    mmSys.mem_bank     = 0;
-    mmSys.fifo_channel = FIFO_MAXMOD;
+    mm_ds_system mmSys =
+    {
+        .mod_count    = 0,
+        .samp_count   = 0,
+        .mem_bank     = 0,
+        .fifo_channel = FIFO_MAXMOD
+    };
     mmInit(&mmSys);
 
     // Open the stream
-    mm_stream stream;
-    stream.sampling_rate = wavHeader.sampleRate;
-    stream.buffer_length = 16000;
-    stream.callback      = streamingCallback;
-    stream.format        = getMMStreamType(wavHeader.numChannels,
-                                           wavHeader.bitsPerSample);
-    stream.timer         = MM_TIMER0;
-    stream.manual        = false;
+    mm_stream stream =
+    {
+        .sampling_rate = wavHeader.sampleRate,
+        .buffer_length = 2048,
+        .callback      = streamingCallback,
+        .format        = getMMStreamType(wavHeader.numChannels, wavHeader.bitsPerSample),
+        .timer         = MM_TIMER0,
+        .manual        = false,
+    };
     mmStreamOpen(&stream);
 
     while (1)
     {
         swiWaitForVBlank();
+
+        // Read file until we have filled the circular buffer
+        streamingFillBuffer(false);
 
         scanKeys();
 
@@ -173,7 +322,8 @@ int main(int argc, char **argv)
 
     mmStreamClose();
 
-    if (fclose(wavFile) != 0) {
+    if (fclose(wavFile) != 0)
+    {
         perror("fclose");
         waitForever();
     }
