34c84f33478ad4f0469061480935de5ca7a14433
W3SLAV <49104904+W3SLAV@users.noreply.github.com>
2025-08-13 17:02:50 -0500

    Update Install_Linux.sh for pacman

--- Install_Linux.sh
+++ Install_Linux.sh
@@ -3,7 +3,12 @@
 #some installed elements need sudo, so to improve user experience run with sudo so you don't have to enter a password 10 times
 #this can be done with sudo bash Install_Linux.sh
 python tools/Orca.py
+
 export PATH=/opt/wonderful/bin:$PATH
 export WONDERFUL_TOOLCHAIN=/opt/wonderful
+ln -s /opt/wonderful/thirdparty/blocksds /opt/blocksds
+export BLOCKSDS=/opt/wonderful/thirdparty/blocksds/core
+export BLOCKSDSEXT=/opt/wonderful/thirdparty/blocksds/external
+
 
 echo "Install finished, but you may need to set the PATH for Wonderful Toolchain permanently"
