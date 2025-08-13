3cbce1ca8d2291050edd5090a5c44b7279daa6fc
W3SLAV <49104904+W3SLAV@users.noreply.github.com>
2025-08-12 20:19:17 -0500

    Add final installer shell script

---
+++ Install_Linux.sh
@@ -0,0 +1,7 @@
+# W3SLAV was x
+
+#some installed elements need sudo, so to improve user experience run with sudo so you don't have to enter a password 10 times
+#this can be done with sudo bash Install_Linux.sh
+python tools/Orca.py
+export PATH=/opt/wonderful/bin:$PATH
+WONDERFUL_TOOLCHAIN=/opt/wonderful
