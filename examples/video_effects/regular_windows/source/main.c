fbd45444e4c8bd5775a95bc3b5ff8a0eef3dc64d
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-12-01 22:25:32 +0000

    examples: Clarify comment in example of regular windows

--- examples/video_effects/regular_windows/source/main.c
+++ examples/video_effects/regular_windows/source/main.c
@@ -27,11 +27,16 @@ int main(int argc, char *argv[])
 
     // Setup two windows. Window 0 has priority over window 1. In this example,
     // windows 0 is a small rectangle in the center of the screen, and window 1
-    // is a rectangle that covers most of the screen. First, the outside of the
-    // big rectangle isn't inside window 1, so the background isn't visible. The
-    // inside of window 1 has enabled the display of the background, so it is
-    // visible. Last, windows 0 has disabled the background, and it takes
-    // priority over window 1, so it isn't visible inside.
+    // is a rectangle that covers most of the screen:
+    //
+    // - The outside of the big rectangle isn't inside window 1, so the
+    //   background isn't visible.
+    //
+    // - The inside of window 1 has enabled the display of the background, so it
+    //   is visible.
+    //
+    // - Windows 0 has disabled the background, and it takes priority over
+    //   window 1, so it isn't visible inside.
     windowSetBounds(WINDOW_0, 48, 48, SCREEN_WIDTH - 48, SCREEN_HEIGHT - 48);
     windowSetBounds(WINDOW_1, 16, 16, SCREEN_WIDTH - 16, SCREEN_HEIGHT - 16);
     bgWindowDisable(bg, WINDOW_0);
