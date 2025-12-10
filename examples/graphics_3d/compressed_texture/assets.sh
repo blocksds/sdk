69925af26117270beeda29fe28a935966e74bc08
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-12-10 18:27:57 +0000

    examples: Add transparent areas to compressed texture
    This way we can see that transparent pixels are actually transparent.

--- examples/graphics_3d/compressed_texture/assets.sh
+++ examples/graphics_3d/compressed_texture/assets.sh
@@ -4,6 +4,10 @@ rm -rf data
 mkdir -p data
 
 # ptexconv can be obtained here: https://github.com/Garhoogin/ptexconv/
+#
+# It can also be installed with wf-pacman:
+#
+#     wf-pacman -Sy blocksds-ptexconv
 
 /opt/blocksds/external/ptexconv/ptexconv \
     -gt -ob -k FF00FF -v -f tex4x4 \
