2d2bf0810d7a223a02494633717a3861fc060416
Antonio Niño Díaz <antonio_nd@proton.me>
2026-08-28 12:48:54 +0200

    examples: tests: testing: Automate more examples and tests

---
+++ examples/graphics_3d/volumetric_shadow/test/run.py
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: CC0-1.0
+#
+# SPDX-FileContributor: Antonio Niño Díaz, 2026
+
+import blocksds_testing as bt
+
+from itertools import repeat
+from libretro import JoypadState, UnformattedLogDriver
+
+bt.delete_all_screenshots()
+
+def generate_input():
+    yield from repeat(0, 40)
+    yield from repeat(JoypadState(select=True), 10)
+
+log_driver = UnformattedLogDriver()
+with bt.session_start(bt.find_rom(), generate_input, log_driver) as session:
+    bt.session_run_frames(session, 30)
+    res_1 = bt.save_screenshot(session, 'result_1.png')
+
+    bt.session_run_frames(session, 20)
+    res_2 = bt.save_screenshot(session, 'result_2.png')
+
+    bt.assert_no_errors_in_logs(log_driver)
+
+bt.compare_image_with_reference(res_1, 'reference_1.png')
+bt.compare_image_with_reference(res_2, 'reference_2.png')
