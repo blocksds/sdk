368094350768eed59a2280db41f8f254b8ac0846
Antonio Niño Díaz <antonio_nd@proton.me>
2026-09-01 01:35:56 +0100

    testing: Automate the assertions example

---
+++ examples/debug/assert/test/run.py
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: CC0-1.0
+#
+# SPDX-FileContributor: Antonio Niño Díaz, 2026
+
+import blocksds_testing as bt
+
+from itertools import repeat
+from libretro import JoypadState, UnformattedLogDriver
+
+rom_path = bt.find_rom()
+bt.delete_all_screenshots()
+
+def generate_input_arm9():
+    yield from repeat(0, 20)
+    yield from repeat(JoypadState(a=True), 10)
+    yield from repeat(0, 20)
+
+def generate_input_arm7():
+    yield from repeat(0, 20)
+    yield from repeat(JoypadState(b=True), 10)
+    yield from repeat(0, 20)
+
+log_driver = UnformattedLogDriver()
+with bt.session_start(rom_path, generate_input_arm9, log_driver) as session:
+    bt.session_run_frames(session, 40)
+    res_1 = bt.save_screenshot(session, 'result_1.png', (0, 192, 256, 384))
+    bt.assert_no_errors_in_logs(log_driver)
+
+log_driver = UnformattedLogDriver()
+with bt.session_start(rom_path, generate_input_arm7, log_driver) as session:
+    bt.session_run_frames(session, 40)
+    res_2 = bt.save_screenshot(session, 'result_2.png', (0, 192, 256, 384))
+    bt.assert_no_errors_in_logs(log_driver)
+
+bt.compare_image_with_reference(res_1, 'reference_1.png')
+bt.compare_image_with_reference(res_2, 'reference_2.png')
