1197cf7f1b6a90cfd43150fe186405fee6e7bc48
Antonio Niño Díaz <antonio_nd@proton.me>
2026-09-04 00:19:46 +0100

    examples: Let user select modifier mode in default keyboard example

--- examples/keyboard/default_keyboard/source/main.c
+++ examples/keyboard/default_keyboard/source/main.c
@@ -22,6 +22,7 @@ int main(int argc, char **argv)
     char string[50];
     string[0] = '\0';
     int string_cursor = 0;
+    KeyboardModifierMode mode = KeyboardModifiersIgnore; // Default
 
     while (1)
     {
@@ -31,9 +32,21 @@ int main(int argc, char **argv)
 
         consoleClear();
 
-        printf("Press START to exit to loader\n");
+        const char *mode_str[] = {
+            // CTRL and ALT are treated like all other buttons. Default behaviour.
+            [KeyboardModifiersIgnore]      = "Don't stick ",
+
+            // CTRL and ALT are held until another key is pressed (not CTRL or ALT).
+            [KeyboardModifiersStickOnce]   = "Stick once  ",
+
+            // CTRL and ALT are held until they are pressed again.
+            [KeyboardModifiersStickAlways] = "Stick always"
+        };
+
+        printf("START: Exit to loader\n");
+        printf("L/R:   CTRL/ALT: %s\n", mode_str[mode]);
         printf("\n");
-        printf("Keyboard test. Space: %zu chars\n", sizeof(string) - 1);
+        printf("Space: %zu chars\n", sizeof(string) - 1);
 
         int16_t c = keyboardUpdate();
         if (c != -1)
@@ -62,8 +75,23 @@ int main(int argc, char **argv)
         printf("[%s]\n", string);
         printf("Length: %d\n", strlen(string));
 
-        if (keysDown() & KEY_START)
+        u16 keys_down = keysDown();
+
+        if (keys_down & KEY_START)
             break;
+
+        if (keys_down & KEY_L)
+        {
+            if (mode > KeyboardModifiersIgnore)
+                mode--;
+            keyboardModifierModeSet(mode);
+        }
+        else if (keys_down & KEY_R)
+        {
+            if (mode < KeyboardModifiersStickAlways)
+                mode++;
+            keyboardModifierModeSet(mode);
+        }
     }
 
     return 0;
