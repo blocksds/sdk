549b35e01c46745e9bdbc43f5ad76c902acf5205
Antonio Niño Díaz <antonio_nd@proton.me>
2026-09-04 01:14:47 +0100

    testing: Add helpers to test the default libnds keyboard

--- scripts/blocksds_testing/blocksds_testing/__init__.py
+++ scripts/blocksds_testing/blocksds_testing/__init__.py
@@ -6,8 +6,9 @@ from .blocksds_testing import (session_start, session_run_frames,
                                string_is_in_logs, assert_no_errors_in_logs,
                                find_rom, delete_all_screenshots,
                                save_screenshot, compare_image_with_reference,
-                               touch)
+                               touch, key_to_coordinates, key_to_touch)
 
 __all__ = ['session_start', 'session_run_frames', 'string_is_in_logs',
            'assert_no_errors_in_logs', 'find_rom', 'delete_all_screenshots',
-           'save_screenshot', 'compare_image_with_reference', 'touch']
+           'save_screenshot', 'compare_image_with_reference', 'touch',
+           'key_to_coordinates', 'key_to_touch']
