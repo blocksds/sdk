e7c8c64526f1acaf0bc557165f682469681b43c6
Antonio Niño Díaz <antonio_nd@proton.me>
2026-09-09 21:31:27 +0100

    testing: Simplify path handling in testing module
    Mixing byte arrays and strings is annoying, so let's stick to strings.

--- scripts/blocksds_testing/blocksds_testing/blocksds_testing.py
+++ scripts/blocksds_testing/blocksds_testing/blocksds_testing.py
@@ -21,17 +21,17 @@ def session_start(game, input_gen=None, log_driver=None, user_options=None, *,
     core_path = os.path.join(blobs_path, 'melondsds_libretro.so')
 
     path_driver = TempDirPathDriver(core_path, 'libretro')
-    testdir = path_driver.root_dir
-    system_dir = path_driver.system_dir
-    playlist_dir = path_driver.playlist_dir
-    save_dir = path_driver.save_dir
+    testdir = path_driver.root_dir.decode("utf-8")
+    system_dir = path_driver.system_dir.decode("utf-8")
+    playlist_dir = path_driver.playlist_dir.decode("utf-8")
+    save_dir = path_driver.save_dir.decode("utf-8")
     save_directory = save_dir
-    savestate_directory = os.path.join(testdir, b'states')
-    core_system_dir = os.path.join(system_dir, b'melonDS DS')
-    core_save_dir = os.path.join(save_dir, b'melonDS DS')
-    wfcsettings_path = os.path.join(core_system_dir, b'wfcsettings.bin')
-    dldi_sd_card_path = os.path.join(core_save_dir, b'dldi_sd_card.bin')
-    dldi_sd_card_sync_path = os.path.join(core_save_dir, b'dldi_sd_card')
+    savestate_directory = os.path.join(testdir, 'states')
+    core_system_dir = os.path.join(system_dir, 'melonDS DS')
+    core_save_dir = os.path.join(save_dir, 'melonDS DS')
+    wfcsettings_path = os.path.join(core_system_dir, 'wfcsettings.bin')
+    dldi_sd_card_path = os.path.join(core_save_dir, 'dldi_sd_card.bin')
+    dldi_sd_card_sync_path = os.path.join(core_save_dir, 'dldi_sd_card')
 
     print('[*] Test dir:', testdir)
 
@@ -54,13 +54,13 @@ def session_start(game, input_gen=None, log_driver=None, user_options=None, *,
     else:
         options = default_options
 
-    if not os.path.exists(os.path.join(system_dir, b'nand.bin')):
-        os.symlink(os.path.join(blobs_path, 'nand.bin'), os.path.join(system_dir, b'nand.bin'))
-        os.symlink(os.path.join(blobs_path, 'dsfirmware.bin'), os.path.join(system_dir, b'dsfirmware.bin'))
-        os.symlink(os.path.join(blobs_path, 'bios7i.bin'), os.path.join(system_dir, b'dsi_bios7.bin'))
-        os.symlink(os.path.join(blobs_path, 'bios9i.bin'), os.path.join(system_dir, b'dsi_bios9.bin'))
-        os.symlink(os.path.join(blobs_path, 'bios7.bin'), os.path.join(system_dir, b'bios7.bin'))
-        os.symlink(os.path.join(blobs_path, 'bios9.bin'), os.path.join(system_dir, b'bios9.bin'))
+    if not os.path.exists(os.path.join(system_dir, 'nand.bin')):
+        os.symlink(os.path.join(blobs_path, 'nand.bin'), os.path.join(system_dir, 'nand.bin'))
+        os.symlink(os.path.join(blobs_path, 'dsfirmware.bin'), os.path.join(system_dir, 'dsfirmware.bin'))
+        os.symlink(os.path.join(blobs_path, 'bios7i.bin'), os.path.join(system_dir, 'dsi_bios7.bin'))
+        os.symlink(os.path.join(blobs_path, 'bios9i.bin'), os.path.join(system_dir, 'dsi_bios9.bin'))
+        os.symlink(os.path.join(blobs_path, 'bios7.bin'), os.path.join(system_dir, 'bios7.bin'))
+        os.symlink(os.path.join(blobs_path, 'bios9.bin'), os.path.join(system_dir, 'bios9.bin'))
 
     mic = None
     if microphone_gen is not None:
