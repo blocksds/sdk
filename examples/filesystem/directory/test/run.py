31e97e8c99fc4f9c27b9a35195a748481add5a2f
Antonio Niño Díaz <antonio_nd@proton.me>
2026-09-10 02:03:39 +0100

    testing: Automate more tests and examples

---
+++ examples/filesystem/directory/test/run.py
@@ -0,0 +1,57 @@
+# SPDX-License-Identifier: CC0-1.0
+#
+# SPDX-FileContributor: Antonio Niño Díaz, 2026
+
+import blocksds_testing as bt
+
+from itertools import repeat
+from libretro import JoypadState, UnformattedLogDriver
+
+bt.delete_all_screenshots()
+
+res = []
+number = 1
+
+def do_screenshot(session):
+    global number
+
+    res.append(bt.save_screenshot(session, f'result_{number}.png'))
+    number = number + 1
+
+# DSi mode, NAND enabled, SD enabled, DLDI enabled
+# ------------------------------------------------
+
+user_options = {
+    "melonds_console_mode": "dsi",
+}
+
+log_driver = UnformattedLogDriver()
+with bt.session_start(bt.find_rom(), None, log_driver, user_options,
+                      user_dldi_root='test/dldi_root',
+                      user_dsi_sd_image='test/dsi_sd_root.img') as session:
+
+    bt.session_run_frames(session, 20)
+    do_screenshot(session)
+
+    bt.assert_no_errors_in_logs(log_driver)
+
+# DS mode, DLDI enabled
+# ---------------------
+
+user_options = {
+    "melonds_console_mode": "ds",
+}
+
+log_driver = UnformattedLogDriver()
+with bt.session_start(bt.find_rom(), None, log_driver, user_options,
+                      user_dldi_root='test/dldi_root') as session:
+
+    bt.session_run_frames(session, 20)
+    do_screenshot(session)
+
+    bt.assert_no_errors_in_logs(log_driver)
+
+# ================================================
+
+for i in range(len(res)):
+    bt.compare_image_with_reference(res[i], f'reference_{i + 1}.png')
